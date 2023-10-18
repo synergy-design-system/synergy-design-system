8c28b023400e4c75232d6d9822daa0079f2d4374
Christian Schilling <christian.schilling@sick.de>
2023-10-18 09:28:47 +0200

    Adjusted paths to address renaming @synergy-design-system/design-tokens -> @synergy-design-system/tokens

--- packages/docs/src/helpers/colors.ts
+++ packages/docs/src/helpers/colors.ts
@@ -1,5 +1,5 @@
 import { paramCase } from 'change-case';
-import * as tokens from '@synergy-design-system/design-tokens';
+import * as tokens from '@synergy-design-system/tokens';
 
 /**
  * Get all colors from a palette as object

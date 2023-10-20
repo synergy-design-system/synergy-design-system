b76aa4d9daa923178488d028cc74cada6d2f40cb
Christian Schilling <christian.schilling@sick.de>
2023-10-20 14:56:17 +0200

    Added basic readme

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,4 +1,4 @@
 export { SynButton } from './components/syn-button/index.js';
 export { SynIcon } from './components/syn-icon/index.js';
-export { SynSpinner } from './components/syn-spinner/index.js';
 export { SynInput } from './components/syn-input/index.js';
+export { SynSpinner } from './components/syn-spinner/index.js';

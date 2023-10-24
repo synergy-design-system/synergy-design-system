0c664277604454971c43e5f0ada2328d3bddb74c
Christian Schilling <christian.schilling@sick.de>
2023-10-24 15:47:28 +0200

    Removed old build scripts from react wrappers (now done directly in components)

--- packages/react/scripts/jobs/tsc.js
+++
@@ -1,6 +0,0 @@
-import { execPromise, job } from './shared.js';
-
-export const runTypeScript = job('Generating typescript types...', async (distDir) => await execPromise(
-  `tsc --project ./tsconfig.prod.json --outdir "${distDir}"`,
-  { stdio: 'inherit' },
-));

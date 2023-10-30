4021ee42843e3173ea3a6b3e8f63e58939071795
Alexandra Mendes <alexandra.mendes@virtual-identity.com>
2023-10-30 10:56:53 +0000

    add more style to syn-input

--- packages/components/src/components/input/input.custom.styles.ts
+++ packages/components/src/components/input/input.custom.styles.ts
@@ -4,4 +4,20 @@ export default css`
     .input--medium .input__control {
     padding: var(--syn-spacing-x-small) var(--syn-input-spacing-small);
   }
+
+    .input--small .input__control {
+    padding: var(--syn-spacing-3x-small) var(--syn-input-spacing-small);
+  }
+
+    .input--large .input__control {
+    padding: var(--syn-spacing-small) var(--syn-input-spacing-medium);
+  }
+
+  .input__prefix ::slotted(*) {
+    padding: var(--syn-spacing-2x-small);
+  }
+
+    .input__suffix ::slotted(*) {
+    padding: var(--syn-spacing-2x-small);
+  }
 `;
--- packages/components/src/styles/form-control.custom.styles.ts
+++ packages/components/src/styles/form-control.custom.styles.ts
@@ -3,8 +3,16 @@ import { css } from 'lit';
 export default css` 
 
    /* Label */
-  .form-control--has-label .form-control__label {
+  .form-control--has-label.form-control--small .form-control__label {
+    margin-bottom: var(--syn-spacing-x-small);
+  }
+  
+  .form-control--has-label.form-control--medium .form-control__label {
     margin-bottom: var(--syn-spacing-small);
+  }
+
+  .form-control--has-label.form-control--large .form-control__label {
+    margin-bottom: var(--syn-spacing-medium);
   }
     /* Help text */
   .form-control--has-help-text .form-control__help-text {

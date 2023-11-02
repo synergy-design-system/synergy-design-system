0b9d5fce31241cc0d5e178d0732ac071406dc274
mariohamann <mario.hamann@virtual-identity.com>
2023-11-02 11:55:38 +0100

    make custom vendorism scripts more readable

--- packages/components/scripts/vendorism/custom/button.vendorism.js
+++ packages/components/scripts/vendorism/custom/button.vendorism.js
@@ -3,52 +3,54 @@ import { removeSection } from '../remove-section.js';
 export default (path, content) => {
   const output = { content, path };
 
-  if (path.includes('button.component.ts') || path.includes('button.styles.ts')) {
-    // Remove unneeded modifiers from component's classMap
-    ['pill', 'circle', 'danger', 'warning', 'success', 'neutral', 'default'].forEach((modifier) => {
-      output.content = removeSection(output.content, `'button--${modifier}':`, ',');
-    });
+  if (!path.includes('button.component.ts') && !path.includes('button.styles.ts')) {
+    return output;
+  }
+  // Remove unneeded modifiers from component's classMap
+  ['pill', 'circle', 'danger', 'warning', 'success', 'neutral', 'default'].forEach((modifier) => {
+    output.content = removeSection(output.content, `'button--${modifier}':`, ',');
+  });
 
-    // Remove pill and circle props from component
-    output.content = removeSection(output.content, '/** Draws a pill-style', ';');
-    output.content = removeSection(output.content, '/**\n   * Draws a circular', ';');
+  // Remove pill and circle props from component
+  output.content = removeSection(output.content, '/** Draws a pill-style', ';');
+  output.content = removeSection(output.content, '/**\n   * Draws a circular', ';');
 
-    // Remove pill and circle from CSS
-    ['Pill', 'Circle'].forEach((modifier) => {
+  // Remove pill and circle from CSS
+  ['Pill', 'Circle'].forEach((modifier) => {
+    output.content = removeSection(
+      output.content,
+      `  /*\n   * ${modifier} modifier`,
+      '  /*',
+      { preserveEnd: true, removePrecedingWhitespace: false },
+    );
+  });
+
+  // Remove all colors except primary from CSS
+  ['Success', 'Warning', 'Danger', 'Neutral', 'Default'].forEach((color) => {
+    // They appear twice in the file
+    [0, 1].forEach(() => {
       output.content = removeSection(
         output.content,
-        `  /*\n   * ${modifier} modifier`,
-        '  /*',
+        `/* ${color}`,
+        '/*',
         { preserveEnd: true, removePrecedingWhitespace: false },
       );
     });
+  });
 
-    // Remove all colors except primary from CSS
-    ['Success', 'Warning', 'Danger', 'Neutral', 'Default'].forEach((color) => {
-      // They appear twice in the file
-      [0, 1].forEach(() => {
-        output.content = removeSection(
-          output.content,
-          `/* ${color}`,
-          '/*',
-          { preserveEnd: true, removePrecedingWhitespace: false },
-        );
-      });
-    });
+  // Use variant prop for "shape"
+  output.content = removeSection(output.content, '@property({ reflect: true }) variant:', ';', { preserveEnd: true, preserveStart: true, removePrecedingWhitespace: false });
+  output.content = removeSection(output.content, '/** Draws an outlined', ';');
+  output.content = output.content.replace('variant:', "variant: 'default' | 'outline' | 'text' = 'default'");
+  output.content = output.content.replace('!this.outline', 'this.variant === \'default\'');
+  output.content = output.content.replace('this.outline', 'this.variant === \'outline\'');
 
-    // Use variant prop for "shape"
-    output.content = removeSection(output.content, '@property({ reflect: true }) variant:', ';', { preserveEnd: true, preserveStart: true, removePrecedingWhitespace: false });
-    output.content = removeSection(output.content, '/** Draws an outlined', ';');
-    output.content = output.content.replace('variant:', "variant: 'default' | 'outline' | 'text' = 'default'");
-    output.content = output.content.replace('!this.outline', 'this.variant === \'default\'');
-    output.content = output.content.replace('this.outline', 'this.variant === \'outline\'');
+  // Set "primary" as default color
+  // If we need more colors later, a "color" prop would have to be added
+  output.content = output.content.replace("this.variant === 'primary'", 'true');
 
-    // Set "primary" as default color
-    // If we need more colors later, a "color" prop would have to be added
-    output.content = output.content.replace("this.variant === 'primary'", 'true');
+  // Rename "standard" class to default
+  output.content = output.content.replace(/button--standard/g, 'button--default');
 
-    // Rename "standard" class to default
-    output.content = output.content.replace(/button--standard/g, 'button--default');
-  }
   return output;
 };
--- packages/components/scripts/vendorism/custom/input.vendorism.js
+++ packages/components/scripts/vendorism/custom/input.vendorism.js
@@ -3,35 +3,35 @@ import { removeSection } from '../remove-section.js';
 export default (path, content) => {
   const output = { content, path };
 
-  if (path.includes('input.component.ts')) {
-    // Pill is not supported in Synergy
-    output.content = removeSection(output.content, "'input--pill':", ',');
-    output.content = removeSection(output.content, '/** Draws a pill-style', 'pill = false;');
-
-    // We need to add classes depenending on prefix and suffix slots to use them in CSS
-    output.content = output.content.replace(
-      "HasSlotController(this, 'help-text', 'label')",
-      "HasSlotController(this, 'help-text', 'label', 'prefix', 'suffix')",
-    );
-    output.content = output.content.replace(
-      "const hasHelpTextSlot = this.hasSlotController.test('help-text');",
-      "const hasHelpTextSlot = this.hasSlotController.test('help-text');\n    const hasPrefixSlot = this.hasSlotController.test('prefix');\n    const hasSuffixSlot = this.hasSlotController.test('suffix');",
-    );
-    output.content = output.content.replace(
-      "'form-control--has-help-text': hasHelpText",
-      "'form-control--has-help-text': hasHelpText,\n          'form-control--has-prefix': hasPrefixSlot,\n          'form-control--has-suffix': hasSuffixSlot",
-    );
+  if (!path.includes('input.component.ts') && !path.includes('input.styles.ts') && !path.includes('input.stories.ts')) {
+    return output;
   }
-
   // We don't provide a filled property in Synergy, but instead use the filled styles for readonly
-  if (path.includes('input.component.ts') || path.includes('input.styles.ts') || path.includes('input.stories.ts')) {
-    // update stories
-    output.content = output.content.replace('<syn-input placeholder="Type something" filled>', '<syn-input value="Readonly content" readonly>');
-    output.content = output.content.replaceAll('Filled', 'Readonly');
+  // update stories
+  output.content = output.content.replace('<syn-input placeholder="Type something" filled>', '<syn-input value="Readonly content" readonly>');
+  output.content = output.content.replaceAll('Filled', 'Readonly');
 
-    // update component and styles
-    output.content = removeSection(output.content, '/** Draws a filled', 'filled = false;');
-    output.content = output.content.replaceAll('filled', 'readonly'); // makes changes in styles and components
-  }
+  // update component and styles
+  output.content = removeSection(output.content, '/** Draws a filled', 'filled = false;');
+  output.content = output.content.replaceAll('filled', 'readonly'); // makes changes in styles and components
+
+  // Pill is not supported in Synergy
+  output.content = removeSection(output.content, "'input--pill':", ',');
+  output.content = removeSection(output.content, '/** Draws a pill-style', 'pill = false;');
+
+  // We need to add classes depenending on prefix and suffix slots to use them in CSS
+  output.content = output.content.replace(
+    "HasSlotController(this, 'help-text', 'label')",
+    "HasSlotController(this, 'help-text', 'label', 'prefix', 'suffix')",
+  );
+  output.content = output.content.replace(
+    "const hasHelpTextSlot = this.hasSlotController.test('help-text');",
+    "const hasHelpTextSlot = this.hasSlotController.test('help-text');\n    const hasPrefixSlot = this.hasSlotController.test('prefix');\n    const hasSuffixSlot = this.hasSlotController.test('suffix');",
+  );
+  output.content = output.content.replace(
+    "'form-control--has-help-text': hasHelpText",
+    "'form-control--has-help-text': hasHelpText,\n          'form-control--has-prefix': hasPrefixSlot,\n          'form-control--has-suffix': hasSuffixSlot",
+  );
+
   return output;
 };

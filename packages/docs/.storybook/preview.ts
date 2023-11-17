b99da7f07f868b6e2a851b462affec1c54d1ab94
Christian Schilling <christian.schilling@sick.de>
2023-11-17 07:58:33 +0100

    Load all components directly in storybook

--- packages/docs/.storybook/preview.ts
+++ packages/docs/.storybook/preview.ts
@@ -1,7 +1,6 @@
 import type { Preview } from "@storybook/web-components";
 import '@synergy-design-system/tokens/themes/light.css';
-
-import { setCustomElementsManifest } from '@storybook/web-components';
+import '@synergy-design-system/components';
 
 import '../src/docs.css';
 import '../../tokens/src/shoelace-fallbacks/_utility.css';

0169cd9a1b320a44eb7efb2fa99de3c8b73b9a41
Christian Schilling <christian.schilling@sick.de>
2023-11-17 09:13:35 +0100

    Add support for disabled styles

--- packages/components/src/components/checkbox/checkbox.custom.styles.ts
+++ packages/components/src/components/checkbox/checkbox.custom.styles.ts
@@ -1,7 +1,24 @@
 import { css } from 'lit';
 
+/**
+ * @todo
+ * - [ ] indeterminate Icon has to be defined in assets
+ * - [ ] Toggles size tokens are currently incorrect
+ *   --syn-toggle-size-small,
+ * --syn-toggle-size-medium: 1.125rem,
+ * --syn-toggle-size-large: 1.375rem
+ */
 export default css`
   .checkbox__control {
     border-radius: 0;
+  }
+
+  /* Disabled */
+  .checkbox--disabled {
+    opacity: 1;
+  }
+
+  .checkbox--disabled .checkbox__label {
+    color: var(--syn-color-neutral-700);
   }
 `;

d0ea2c8751b7e24887d187ccc4cf2348d20e20fa
Christian Schilling <christian.schilling@sick.de>
2023-11-17 10:21:52 +0100

    Use design token for border-radius and label spacing

--- packages/components/src/components/checkbox/checkbox.custom.styles.ts
+++ packages/components/src/components/checkbox/checkbox.custom.styles.ts
@@ -2,15 +2,21 @@ import { css } from 'lit';
 
 /**
  * @todo
+ * - [ ] :hover Design not in figma
  * - [ ] indeterminate Icon has to be defined in assets
  * - [ ] Toggles size tokens are currently incorrect
- *   --syn-toggle-size-small,
- * --syn-toggle-size-medium: 1.125rem,
- * --syn-toggle-size-large: 1.375rem
+ *        --syn-toggle-size-small,
+ *        --syn-toggle-size-medium: 1.125rem,
+ *        --syn-toggle-size-large: 1.375rem
+ *        --syn-focus-ring-offset: 0
  */
 export default css`
   .checkbox__control {
-    border-radius: 0;
+    border-radius: var(--syn-input-border-radius-small);
+  }
+
+  .checkbox__label {
+    margin-inline-start: var(--syn-spacing-x-small);
   }
 
   /* Disabled */

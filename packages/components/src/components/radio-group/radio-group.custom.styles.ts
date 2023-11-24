12c393de5f9a6622bbd09865b376dc2e05a2cc94
Alexandra Mendes <alexandra.mendes@virtual-identity.com>
2023-11-24 07:59:29 +0000

    fix slotted selector

--- packages/components/src/components/radio-group/radio-group.custom.styles.ts
+++ packages/components/src/components/radio-group/radio-group.custom.styles.ts
@@ -1,7 +1,7 @@
 import { css } from 'lit';
 
 export default css`
-  .radio-group--required ::slotted(*) {
+  .radio-group ::slotted(syn-radio) {
     margin-bottom: var(--syn-spacing-x-small);
   }
 `;

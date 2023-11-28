7d80a43898246240b8a93f479cc8973785974f70
Susanne Kirchner <susanne.kirchner@sick.de>
2023-11-28 10:50:48 +0100

    Add icon stories with all available icons

--- packages/docs/stories/components/icon.stories.ts
+++ packages/docs/stories/components/icon.stories.ts
@@ -2,7 +2,7 @@
 /* eslint-disable import/no-relative-packages */
 
 import '../../../components/src/components/icon/icon';
-import type { Meta, StoryObj } from '@storybook/web-components';
+import { type Meta, type StoryObj } from '@storybook/web-components';
 import { html } from 'lit';
 import { storybookDefaults, storybookHelpers, storybookTemplate } from '../../src/helpers/component.js';
 import { registerIconLibrary } from '../../../components/src/utilities/icon-library.js';
@@ -340,3 +340,49 @@ export const BundledIconLibrary: Story = {
 </div>`;
   },
 };
+
+
+const createIconPage = (letter: string): Story => {
+  return {
+    render: () => {
+      const regex = new RegExp(`^${letter}`);
+      const iconKeys = Object.keys(defaultIcons);
+      const iconContainer = iconKeys.filter((key) => key.match(regex)).map((key) =>
+      (html`
+      <div style="display: flex; flex-direction: column; align-items: center; cursor: pointer"  >
+        <span style="font-size: 12px">${key}</span>
+        <syn-icon style="font-size: 32px" name="${key}"></syn-icon>
+      </div>
+      `));
+      return html`<div style="display: grid; grid-template-columns: repeat(auto-fill, 150px); column-gap: 12px; row-gap: 32px;">${iconContainer}</div>`;
+    }
+  }
+}
+
+export const Number_Icons = createIconPage('[0-9]');
+export const A_Icons = createIconPage('a');
+export const B_Icons = createIconPage('b');
+export const C_Icons = createIconPage('c');
+export const D_Icons = createIconPage('d');
+export const E_Icons = createIconPage('e');
+export const F_Icons = createIconPage('f');
+export const G_Icons = createIconPage('g');
+export const H_Icons = createIconPage('h');
+export const I_Icons = createIconPage('i');
+export const J_Icons = createIconPage('j');
+export const K_Icons = createIconPage('k');
+export const L_Icons = createIconPage('l');
+export const M_Icons = createIconPage('m');
+export const N_Icons = createIconPage('n');
+export const O_Icons = createIconPage('o');
+export const P_Icons = createIconPage('p');
+export const Q_Icons = createIconPage('q');
+export const R_Icons = createIconPage('r');
+export const S_Icons = createIconPage('s');
+export const T_Icons = createIconPage('t');
+export const U_Icons = createIconPage('u');
+export const V_Icons = createIconPage('v');
+export const W_Icons = createIconPage('w');
+export const X_Icons = createIconPage('x');
+export const Y_Icons = createIconPage('y');
+export const Z_Icons = createIconPage('z');

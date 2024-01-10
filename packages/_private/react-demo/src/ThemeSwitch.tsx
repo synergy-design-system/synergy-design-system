4bd8340789885779d597c04eb86e099cea75b586
Christian Schilling <christian.schilling@sick.de>
2024-01-10 09:49:23 +0100

    Added theme switcher for react

---
+++ packages/_private/react-demo/src/ThemeSwitch.tsx
@@ -0,0 +1,33 @@
+import type { SynButton as SynButtonElement } from '@synergy-design-system/components';
+import { SynButton } from '@synergy-design-system/react';
+import { MouseEventHandler } from 'react';
+
+const switchTheme: MouseEventHandler<SynButtonElement> = (e) => {
+  const bdy = document.body;
+  const btn = (e.target as SynButtonElement);
+  const currentTheme = bdy.classList.contains('syn-theme-dark') ? 'dark' : 'light';
+
+  if (currentTheme === 'light') {
+    // Light theme
+    bdy.classList.remove('syn-theme-light');
+    bdy.classList.add('syn-theme-dark');
+    btn.innerText = 'Switch to light theme';
+  } else {
+    // Dark theme
+    bdy.classList.remove('syn-theme-dark');
+    bdy.classList.add('syn-theme-light');
+    btn.innerText = 'Switch to dark theme';
+  }
+
+  // Will toggle between light to dark
+  // with each call to switchTheme
+  console.log(bdy.className);
+};
+
+export const ThemeSwitch = () => (
+  <header>
+    <SynButton onClick={switchTheme}>
+      Switch Theme
+    </SynButton>
+  </header>
+);

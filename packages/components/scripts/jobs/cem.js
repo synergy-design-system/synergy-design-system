eda5acc35f26bbfcb5933a4f5a11a734901e7c85
Christian Schilling <christian.schilling@sick.de>
2024-01-11 09:39:07 +0100

    Added automatic formatting for version updates and vendorism

--- packages/components/scripts/jobs/cem.js
+++ packages/components/scripts/jobs/cem.js
@@ -1,8 +1,4 @@
-import fs from 'fs/promises';
-import prettier from 'prettier';
-import { execPromise, getPath, job } from './shared.js';
-// eslint-disable-next-line import/no-relative-packages
-import prettierConfig from '../../../../prettier.config.js';
+import { execPromise, formatFile, getPath, job } from './shared.js';
 
 /**
  * Run cem for the components package
@@ -13,12 +9,5 @@ export const runCem = job('Creating component manifest...', async () => {
     { stdio: 'inherit' },
   );
 
-  const packageJSON = getPath('../package.json');
-  const packageData = await fs.readFile(packageJSON);
-  const formattedPackageJSON = await prettier.format(packageData.toString(), {
-    ...prettierConfig,
-    parser: 'json-stringify',
-  });
-
-  await fs.writeFile(packageJSON, formattedPackageJSON);
+  await formatFile(getPath('../package.json'), 'json-stringify');
 });

2c74d1df2040ed4ccb51ce15be8c4cd647e97f36
Christian Schilling <christian.schilling@sick.de>
2024-01-25 13:41:20 +0100

    Adjusted paddings, draw translations from static texts file

--- packages/docs/stories/templates/form.stories.tsx
+++ packages/docs/stories/templates/form.stories.tsx
@@ -7,14 +7,11 @@ import {
   Title,
 } from '@storybook/blocks';
 import { html } from 'lit';
+import { getLabel, getTranslation } from '../../src/translations.js';
 
 const meta: Meta = {
   parameters: {
     docs: {
-      story: {
-        inline: false,
-        iframeHeight: 1500,
-      },
       page: () => (
         <>
           <Title />
@@ -23,6 +20,10 @@ const meta: Meta = {
           <Stories />
         </>
       ),
+      story: {
+        iframeHeight: 1600,
+        inline: false,
+      },
     },
   },
   title: 'Templates/Forms',
@@ -34,16 +35,15 @@ export const ContactForm = {
     <style>
     .synergy-form-demo {
       background: var(--syn-color-neutral-0);
-      border: 1px dashed var(--syn-color-neutral-300);
       margin: 0 auto;
-      padding: 10px 20px;
+      padding: var(--syn-spacing-x-large);
       max-width: 750px;
     }
 
     h1 {
       font-size: var(--syn-font-size-3x-large);
       font-weight: normal;
-      margin: 0 0 10px 0;
+      margin: 0 0 var(--syn-spacing-medium) 0;
     }
 
     .synergy-form-demo > p {
@@ -63,27 +63,27 @@ export const ContactForm = {
 
     fieldset legend {
       font-size: var(--syn-font-size-large);
-      margin-bottom: var(--syn-font-size-small);
+      margin-bottom: var(--syn-spacing-medium);
     }
 
     fieldset legend + p {
       font-size: var(--syn-font-size-medium);
-      margin: 0 0 var(--syn-font-size-medium) 0;
+      margin: 0 0 var(--syn-font-size-large) 0;
     }
 
     .fields {
       container-type: inline-size;
       display: flex;
       flex-flow: wrap;
-      margin-bottom: var(--syn-font-size-x-large);
-      gap: var(--syn-spacing-large) var(--syn-font-size-small);
+      margin-bottom: var(--syn-spacing-2x-large);
+      gap: var(--syn-spacing-large) var(--syn-spacing-large);
     }
 
     .fields > * {
       flex-basis: 100%;
     }
 
-    @container (min-width: 700px) {
+    @container (min-width: 640px) {
       .fields > * {
         flex-basis: calc(50% - var(--syn-spacing-medium));
       }
@@ -101,82 +101,86 @@ export const ContactForm = {
       display: grid;
     }
 
-    @container (min-width: 700px) {
+    @container (min-width: 640px) {
       syn-radio-group::part(form-control-input) {
         grid-auto-flow: column;
-        grid-template-rows: 1fr 1fr 1fr;
+        grid-template-rows: 1fr 1fr 1fr 1fr;
       }
     }
 
     .submit-actions {
       display: flex;
       justify-content: right;
-      margin-bottom: var(--syn-font-size-x-large);
+      margin-bottom: var(--syn-spacing-2x-large);
     }
     </style>
     <div class="synergy-form-demo">
-      <h1>Kontaktformular</h1>
-      <p>Bitte geben Sie Ihre Kontaktdaten an und beschreiben Sie, wobei wir Sie unterstützen können.</p>
+      <h1>${getTranslation('contactForm.headline')}</h1>
+      <p>${getTranslation('contactForm.subHeadline')}</p>
 
       <form method="post" id="syn-form-demo">
         
         <fieldset>
           <syn-radio-group
-            label="Zu welchem Thema haben Sie Fragen?"
+            label="${getLabel('contactForm.topicLabel')}"
             name="topic"
             required
           >
-            <syn-radio value="1">Anfrage/Angebot</syn-radio>
-            <syn-radio value="2">Aufträge/Rechnungen</syn-radio>
-            <syn-radio value="3">Rücksendungen/Reklamation</syn-radio>
-            <syn-radio value="4">Zubehörauswahl</syn-radio>
-            <syn-radio value="5">Applikationserklärung</syn-radio>
-            <syn-radio value="6">Inbetriebnahmeunterstützung</syn-radio>
+            ${(getTranslation('contactForm.topics') as string[]).map((topic, index) => html`
+              <syn-radio value=${index}>${topic}</syn-radio>
+            `)}
           </syn-radio-group>
         </fieldset>
 
         <fieldset>
-          <legend>Beschreiben Sie Ihre Anfrage</legend>
+          <legend>${getLabel('contactForm.requestLabel')}</legend>
           <p>
-            Es ist sehr hilfreich, wenn die Beschreibung möglichst genau ist, so können wir noch spezieller auf Ihre Fragen eingehen.
-            (Bei Applikationsbeschreibungen ggf. Material/Abmessungen/Geschwindigkeiten mit angeben).
+            ${getLabel('contactForm.requestContent')}
           </p>
 
-          <syn-textarea label="Ihre Nachricht" name="message" required></syn-textarea>
+          <syn-textarea
+            label="${getLabel('contactForm.messageLabel')}"
+            name="message"
+            required
+          ></syn-textarea>
         </fieldset>
 
         <fieldset>
-          <legend>Ihre Kontaktdaten:</legend>
+          <legend>${getTranslation('contactForm.contactDetailsLabel')}</legend>
           <div class="fields">
-            <syn-input label="Kundennummer" name="customerNr"></syn-input>
-            <syn-input label="Firmenname" name="companyName" required></syn-input>
-            <syn-input label="Adresse" name="address" required></syn-input>
-            <syn-input label="PLZ" name="zip" required></syn-input>
-            <syn-input label="Ort" name="city" required></syn-input>
-            <syn-input label="Land" name="country" required></syn-input>
-            <syn-input label="Ansprechpartner" name="salesPerson" required></syn-input>
-            <syn-input type="tel" label="Telefonnummer" name="phone"></syn-input>
-            <syn-input type="tel" label="Fax" name="fax"></syn-input>
-            <syn-input type="email" label="E-Mail-Adresse" name="mail" required></syn-input>
+            <syn-input label="${getLabel('contactForm.customerNumberLabel')}" name="customerNr"></syn-input>
+            <syn-input label="${getLabel('contactForm.companyNameLabel')}" name="companyName" required></syn-input>
+            <syn-input label="${getLabel('contactForm.addressLabel')}" name="address" required></syn-input>
+            <syn-input label="${getLabel('contactForm.zipLabel')}" name="zip" required></syn-input>
+            <syn-input label="${getLabel('contactForm.cityLabel')}" name="city" required></syn-input>
+            <syn-input label="${getLabel('contactForm.countryLabel')}" name="country" required></syn-input>
+            <syn-input label="${getLabel('contactForm.referenceContactLabel')}" name="salesPerson" required></syn-input>
+            <syn-input type="tel" label="${getLabel('contactForm.phoneLabel')}" name="phone"></syn-input>
+            <syn-input type="tel" label="${getLabel('contactForm.faxLabel')}" name="fax"></syn-input>
+            <syn-input type="email" label="${getLabel('contactForm.emailLabel')}" name="mail" required></syn-input>
           </div>
           <syn-checkbox name="subscribeNewsletter">
-            Ja, ich möchte regelmäßig aktuelle und interessante Informationen zu Lösungen mit Produkten, Systemen und Services von SICK per E-Mail erhalten.
-            Ich kann die Einwilligung jederzeit widerrufen.
-            Für einen Widerruf der Einwilligung kann ich jederzeit den Abmeldelink in jedem Newsletter/jeder E-Mail nutzen.
+            ${getLabel('contactForm.newsletterLabel')}
           </syn-checkbox>
         </fieldset>
 
         <div class="submit-actions">
-          <syn-button type="submit" variant="filled">Absenden</syn-button>
+          <syn-button type="submit" variant="filled">${getLabel('contactForm.submitLabel')}</syn-button>
         </div>
 
         <em>
-          Mit einem * gekennzeichnete Felder sind Pflichtfelder.
-          Ihre Daten werden vertraulich behandelt und selbstverständlich nicht an Dritte weitergegeben.
-          Hier erfahren Sie mehr zu unserem Datenschutz
+          ${getLabel('contactForm.requiredFieldInfo')}
         </em>
       </form>
     </div>
+
+    <script>
+    document.querySelector('form').addEventListener('submit', (e) => {
+      e.preventDefault();
+      const fd = new FormData(e.target);
+      console.log(...fd);
+    })
+    </script>
   `,
 };
 

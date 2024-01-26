17a1fe0efdfb7e3c40bb6da62825feced4bea8cc
Christian Schilling <christian.schilling@sick.de>
2024-01-26 09:13:38 +0100

    chore: 🔧 Adjust test runs so they use the latest version of the bundle for components (#280)

---
+++ packages/components/scripts/tests/index.js
@@ -0,0 +1,64 @@
+import { execSync } from 'child_process';
+import * as jobs from '../jobs/index.js';
+import { getPackageJSONAsObject, getPath } from '../jobs/shared.js';
+
+const componentDir = getPath('../');
+const outDir = getPath('../dist');
+const temporaryOutDir = getPath('../dist.testrunner');
+
+const { version } = getPackageJSONAsObject();
+
+// eslint-disable-next-line no-underscore-dangle
+const __PACKAGE_VERSION__ = JSON.stringify(version.toString());
+
+const synRefreshComponentBundle = () => {
+  const name = 'syn-refresh-component-bundle';
+
+  const rebuildSynergy = async () => {
+    // Create the build in its own directory
+    await jobs.runPrepare(temporaryOutDir);
+    await jobs.runCreateEvents(componentDir);
+    await jobs.runCreateExports(componentDir);
+    await jobs.runTypeScript(temporaryOutDir, './tsconfig.prod.json');
+    await jobs.runEsBuildComponents(temporaryOutDir, __PACKAGE_VERSION__);
+
+    // Finally, remove the dist folder completely
+    // and move the contents of the temporary folder into it
+    await jobs.runPrepare(outDir);
+    execSync(`mv ${temporaryOutDir}/* ${outDir}`);
+  };
+
+  return {
+    name,
+    serverStart: async ({ fileWatcher }) => {
+      // Make sure we always have correct initial data by running the build
+      await rebuildSynergy();
+
+      // Whenever one of our component files has changed,
+      // make sure to recreate our bundle
+      fileWatcher.add('./src/components/**/!(*.(test)).ts');
+
+      // Make sure to only rebuild our components if a component and its associates did change
+      fileWatcher.on('change', async (changedFile) => {
+        const isATest = changedFile.endsWith('.test.ts');
+        const isADistFile = changedFile.includes('dist/');
+
+        // Skip the tests, they should do nothing here
+        // Make sure to skip the currently generated bundle.
+        // It will lead to recursive errors otherwise!
+        if (isATest || isADistFile) {
+          return;
+        }
+
+        await rebuildSynergy();
+      });
+    },
+    serverStop: async () => {
+      await jobs.runPrepare(temporaryOutDir);
+    },
+  };
+};
+
+export default {
+  plugins: [synRefreshComponentBundle()],
+};
--- packages/components/scripts/vendorism/custom/webTestRunner.vendorism.js
+++ packages/components/scripts/vendorism/custom/webTestRunner.vendorism.js
@@ -11,9 +11,22 @@ export const vendorWebTestRunnerConfig = (path, content) => {
 
   // Adjust the path to the theme to make sure we always fetch
   // the latest version from the package
-  const nextContent = content.replaceAll(
+  let nextContent = content.replaceAll(
     'dist/themes/light.css',
     'node_modules/@synergy-design-system/tokens/dist/themes/light.css',
+  );
+
+  // Add the synergy test plugins to the mix
+  nextContent = nextContent.replaceAll(
+    "runner-playwright';",
+    `runner-playwright';
+import synTestPlugins from './scripts/tests/index.js';`,
+  );
+
+  nextContent = nextContent.replaceAll(
+    'esbuildPlugin({',
+    `...synTestPlugins.plugins,
+    esbuildPlugin({`,
   );
 
   return {

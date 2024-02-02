6858813ac5e35707f314300a36071780abb634a1
Christian Schilling <christian.schilling@sick.de>
2024-02-02 11:12:19 +0100

    Added angular support for form

---
+++ packages/_private/angular-demo/src/themeswitch/themeswitch.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import type {
+  SynChangeEvent,
+  SynSwitch as SynSwitchElement,
+} from '@synergy-design-system/components';
+
+@Component({
+  selector: 'theme-switch',
+  templateUrl: './themeswitch.component.html',
+})
+export class ThemeSwitchComponent {
+
+  currentTheme: string = '🌙';
+
+  switchTheme(e: SynChangeEvent) {
+    const { body } = document;
+    const { checked } = e.target as SynSwitchElement;
+    const theme = checked ? 'dark' : 'light';
+    body.classList.remove('syn-theme-light', 'syn-theme-dark');
+    body.classList.add(`syn-theme-${theme}`);
+
+    this.currentTheme = theme === 'light' ? '🌙' : '☀️';
+  }
+}

8741bb673048223895d9dda0aacda24f442f31a9
Christian Schilling <christian.schilling@sick.de>
2024-02-07 09:42:10 +0100

    Added review suggestions

--- packages/components/scripts/jobs/vue/tsc.js
+++ packages/components/scripts/jobs/vue/tsc.js
@@ -6,6 +6,6 @@ import { execPromise, job } from '../shared.js';
  * @param {String} baseDir The base directory path
  */
 export const runVueTypeScript = job('Vue: Generating typescript types...', async (outDir, baseDir) => await execPromise(
-  `vue-tsc --project ${baseDir}/tsconfig.json --declaration --outDir "${outDir}"`,
+  `vue-tsc --project ${baseDir}/tsconfig.json --declaration --emitDeclarationOnly --noEmit false --outDir "${outDir}"`,
   { stdio: 'inherit' },
 ));

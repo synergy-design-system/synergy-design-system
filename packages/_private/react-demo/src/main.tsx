49b29f75860c7895eea6f889271c6f18855fe191
Christian Schilling <christian.schilling@sick.de>
2024-02-21 14:52:38 +0100

    Switched react demo to @synergy-design-system/styles

--- packages/_private/react-demo/src/main.tsx
+++ packages/_private/react-demo/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import '@synergy-design-system/tokens/themes/dark.css';
-import '@synergy-design-system/tokens/themes/light.css';
+import '@synergy-design-system/styles';
 import './app.css';
 import { App } from './App';
 

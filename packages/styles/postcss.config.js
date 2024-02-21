0f8f3e72c9d69edbc698d9629934746194ba2653
Christian Schilling <christian.schilling@sick.de>
2024-02-21 14:49:15 +0100

    Automatically generate fouc styles to scope them to synergy

--- packages/styles/postcss.config.js
+++ packages/styles/postcss.config.js
@@ -1,7 +1,58 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { readFileSync } from 'fs';
+import { readFileSync, readdirSync, writeFileSync } from 'fs';
 import atImport from 'postcss-import';
 import header from 'postcss-header';
+
+/**
+ * We have to create the fouc detection manually to make sure it still works when using
+ * angular, vue or other web-component libraries.
+ */
+const handleFoundPrevention = () => {
+  const foucComponentSelector = readdirSync('../components/src/components')
+    .sort()
+    .map(component => `syn-${component}:not(:defined)`.toLowerCase())
+    .join(',\n');
+
+  const foucFileContents = `
+/**
+ * This file prevents the flash of unstyled components
+ * @see https://web.dev/custom-elements-v1/#styling-a-custom-element
+ * @todo: Create a better matching version after the first components are ready!
+ * @todo: Each component should be defined using its default height and display type.
+ *
+ * This file is created automatically in the build process!
+ * All changes applied will get lost!
+ * To recreate it, please use "pnpm build"!
+ */
+${foucComponentSelector} {
+  display: inline-block;
+  opacity: 0;
+  transition: opacity 0.3s ease-in-out;
+ }
+
+/**
+ * @example:
+ * This example shows how we could provide defaults for the syn-button
+ * in all three sizes.
+ */
+
+/*
+  syn-button[size="small"]:not(:defined) {
+    height: 30px;
+  }
+
+  syn-button[size="medium"]:not(:defined) {
+    height: 40px;
+  }
+
+  syn-button[size="large"]:not(:defined) {
+    height: 50px;
+  }
+*/
+`.trimStart();
+
+  writeFileSync('./src/modules/fouc.css', foucFileContents);
+};
 
 const packageJSON = readFileSync('./package.json', {
   encoding: 'utf-8',
@@ -17,11 +68,13 @@ const banner = `
  */
 `.trim();
 
+handleFoundPrevention();
+
 /** @type {import('postcss-load-config').Config} */
 export default {
   plugins: [
     atImport({
-      allowDuplicates: true,
+      allowDuplicates: false,
       filter: path => !path.startsWith('b64---'), // Prevent inlined items to be imported
     }),
     header({

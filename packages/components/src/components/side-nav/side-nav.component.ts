f66f79293af3d1a07befc10b4026381240efbc5c
Susanne Kirchner <susanne.kirchner@sick.de>
2024-03-11 11:53:54 +0100

    Add side-nav rail mode mouse and touch handling

--- packages/components/src/components/side-nav/side-nav.component.ts
+++ packages/components/src/components/side-nav/side-nav.component.ts
@@ -9,27 +9,28 @@ import styles from './side-nav.styles.js';
 import SynDrawer from '../drawer/drawer.component.js';
 import type SynNavItem from '../nav-item/nav-item.component.js';
 import SynDivider from '../divider/divider.component.js';
+import type { SynRequestCloseEvent } from '../../events/events.js';
 
 /**
- * @summary The <syn-side-nav /> element contains secondary navigation and fits below the header.
+* @summary The <syn-side-nav /> element contains secondary navigation and fits below the header.
  *
- * @status stable
- * @since 1.11.0
+* @status stable
+* @since 1.11.0
  *
- * @dependency syn-divider
- * @dependency syn-drawer
+* @dependency syn-divider
+* @dependency syn-drawer
  *
- * @slot - The main content of the side-nav. Used for <syn-nav-item /> elements.
- * @slot footer - The footer content of the side-nav. Used for <syn-nav-item /> elements.
- *    Please avoid having to many nav-items as it can massively influence the user experience.
+* @slot - The main content of the side-nav. Used for <syn-nav-item /> elements.
+* @slot footer - The footer content of the side-nav. Used for <syn-nav-item /> elements.
+*    Please avoid having to many nav-items as it can massively influence the user experience.
  *
- * @csspart base - The components base wrapper
- * @csspart drawer - The drawer that is used under the hood for creating the side-nav
- * @csspart content-container - The components main content container
- * @csspart content - The components main content
- * @csspart footer-container - The components footer content container
- * @csspart footer-divider - The components footer divider
- * @csspart footer - The components footer content
+* @csspart base - The components base wrapper
+* @csspart drawer - The drawer that is used under the hood for creating the side-nav
+* @csspart content-container - The components main content container
+* @csspart content - The components main content
+* @csspart footer-container - The components footer content container
+* @csspart footer-divider - The components footer divider
+* @csspart footer - The components footer content
  *
  */
 export default class SynSideNav extends SynergyElement {
@@ -42,33 +43,48 @@ export default class SynSideNav extends SynergyElement {
 
   private readonly hasSlotController = new HasSlotController(this, '[default]', 'footer');
 
+  private isInitial: boolean = true;
+
+  constructor() {
+    super();
+    this.handleMouseEnter = this.handleMouseEnter.bind(this);
+    this.handleMouseLeave = this.handleMouseLeave.bind(this);
+  }
+
   /**
-   * Reference to the default slot.
+* Reference to the default slot.
    */
   @query('slot:not([name])') defaultSlot: HTMLSlotElement;
 
   /**
-   * Reference to the footer slot.
+* Reference to the footer slot.
    */
   @query('slot[name="footer"]') footerSlot: HTMLSlotElement;
 
   /**
-   * State if all nav-items have a prefix icon.
+* Reference to the drawer
+   */
+  @query('.side-nav__drawer') drawer: SynDrawer;
+
+  /**
+* State if all nav-items have a prefix icon.
    */
   @state() private hasPrefixIcons = false;
 
+  @state() private hasHoverOnRail = false;
+
   /**
-  * Use the rail attribute to only show the prefix of navigation items.
-  * This will open on hover on the rail navigation.
-  * On touch devices the navigation opens on click and shows an overlay.
+* Use the rail attribute to only show the prefix of navigation items.
+* This will open on hover on the rail navigation.
+* On touch devices the navigation opens on click and shows an overlay.
   *
-  * Note: The Rail is only an option if all Navigation Items on the first level have an Icon.
-  * If this is not the case you should use a burger navigation.
+* Note: The Rail is only an option if all Navigation Items on the first level have an Icon.
+* If this is not the case you should use a burger navigation.
    */
   @property({ reflect: true, type: Boolean }) rail = false;
 
   /**
-   * Get all nav-items from the default and footer slot.
+* Get all nav-items from the default and footer slot.
    */
   private getAllNavItems(): SynNavItem[] {
     const navItemsAll: SynNavItem[] = [];
@@ -91,9 +107,10 @@ export default class SynSideNav extends SynergyElement {
   }
 
   /**
-   * This function handles the rail mode.
-   * Rail mode is only valid if every nav-item has a syn-icon as prefix.
+* This function handles the rail mode.
+* Rail mode is only valid if every nav-item has a syn-icon as prefix.
    */
+  // eslint-disable-next-line complexity
   private handleRailMode() {
     const navItems = this.getAllNavItems();
 
@@ -105,19 +122,17 @@ export default class SynSideNav extends SynergyElement {
     this.hasPrefixIcons = navItems.length !== 0 && itemsWithPrefixIcon.length === navItems.length;
 
     // Only do rail mode, if every nav-item has a syn-icon as prefix
-    if (this.hasPrefixIcons && this.rail) {
-      itemsWithPrefixIcon.forEach((navItem: SynNavItem) => {
-        // eslint-disable-next-line no-param-reassign
-        navItem.open = false;
-      });
+    // TODO: do we only want to allow syn-icon as prefix or also other elements?
+    if (this.hasPrefixIcons && this.rail && !this.hasHoverOnRail) {
       if (!this.querySelector('style.hide-parts-style')) {
         const partsHideStyle = document.createElement('style');
         partsHideStyle.className = 'hide-parts-style';
         partsHideStyle.textContent = `syn-nav-item::part(content-container),
-      syn-nav-item::part(suffix),
-      syn-nav-item::part(chevron) {
-        display: none
-      }`;
+        syn-nav-item::part(suffix),
+        syn-nav-item::part(chevron),
+        syn-nav-item [slot="children"] {
+          display: none
+        }`;
         this.append(partsHideStyle);
       }
     } else {
@@ -125,13 +140,41 @@ export default class SynSideNav extends SynergyElement {
     }
   }
 
+  private handleMouseEnter() {
+    if (this.rail && this.hasPrefixIcons) {
+      this.hasHoverOnRail = true;
+    }
+  }
+
+  private handleMouseLeave() {
+    if (this.rail && this.hasPrefixIcons) {
+      this.hasHoverOnRail = false;
+    }
+  }
+
+  /**
+* Prevent drawer from being closed.
+* @param event - The requested close event emitted from the drawer
+   */
+  private handleRequestClose(event: SynRequestCloseEvent) {
+    event.preventDefault();
+    this.hasHoverOnRail = false;
+  }
+
   /* eslint-disable complexity */
   render() {
+    if (this.isInitial && this.drawer) {
+      this.isInitial = false;
+      this.drawer.shadowRoot!.querySelector('.drawer__panel')?.addEventListener('mouseenter', this.handleMouseEnter);
+      this.drawer.shadowRoot!.querySelector('.drawer__panel')?.addEventListener('mouseleave', this.handleMouseLeave);
+    }
+    const isTouch = window.navigator.maxTouchPoints > 0 || !!('ontouchstart' in window);
+
     const hasFooter = this.hasSlotController.test('footer');
     this.handleRailMode();
+
     /* eslint-disable lit/no-invalid-html */
     /* eslint-disable @typescript-eslint/unbound-method */
-
     return html`
       <nav
         class=${classMap({
@@ -139,6 +182,8 @@ export default class SynSideNav extends SynergyElement {
           'side-nav--has-footer': hasFooter,
           'side-nav--has-prefix-icons': this.hasPrefixIcons,
           'side-nav--rail': this.rail,
+          'side-nav--rail-hover': this.hasHoverOnRail,
+          'side-nav--rail-touch': isTouch,
         })}
         part="base"
       >
@@ -150,10 +195,11 @@ export default class SynSideNav extends SynergyElement {
           open
           part="drawer"
           placement="start"
+          @syn-request-close=${this.handleRequestClose}          
         >
-          <div part="content-container" class="side-nav__content-container">
+          <main part="content-container" class="side-nav__content-container">
             <slot part="content" @slotchange=${this.handleRailMode}></slot>
-          </div>
+          </main>
 
           
           <footer class="side-nav__footer" part="footer-container" slot="footer">  
--- packages/components/src/components/side-nav/side-nav.styles.ts
+++ packages/components/src/components/side-nav/side-nav.styles.ts
@@ -2,21 +2,42 @@ import { css } from 'lit';
 
 export default css`
   :host {
-    display: contents;
+    display: block;
   }
 
-
+  /**
+  * Syn-drawer styling
+  */
 
   syn-drawer::part(body), syn-drawer::part(footer) {
     padding: 0;
   }
 
-  .side-nav--has-prefix-icons.side-nav--rail syn-drawer {
-    --size: 72px;
+  /** 
+  * Overlay 
+  */
+
+  .side-nav__drawer::part(overlay){
+    position: absolute;
   }
+
+ /**
+  * Footer divider
+  */
 
   .side-nav__footer-divider{
     --spacing: 0;
     --color: var(--syn-color-neutral-300);
+  }
+
+  /**
+  * Rail mode
+  */
+  .side-nav--has-prefix-icons.side-nav--rail:not(.side-nav--rail-hover) .side-nav__drawer {
+    --size: 72px;
+  }
+
+  .side-nav--rail-touch.side-nav--rail-hover .side-nav__drawer::part(overlay){
+    display: block;
   }
 `;

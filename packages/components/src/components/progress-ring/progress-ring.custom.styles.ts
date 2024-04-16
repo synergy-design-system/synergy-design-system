666faff38bd4b6929ef9009b0676ba48b9f32d9a
Christian Schilling <christian.schilling@sick.de>
2024-04-16 14:09:58 +0200

    Added style adjustments

--- packages/components/src/components/progress-ring/progress-ring.custom.styles.ts
+++ packages/components/src/components/progress-ring/progress-ring.custom.styles.ts
@@ -1,5 +1,17 @@
 import { css } from 'lit';
 
 export default css`
-  /* Write custom CSS here */
+  :host {
+    --size: 120px;
+    --track-width: var(--syn-spacing-x-small);
+  }
+
+  .progress-ring__indicator {
+    stroke-linecap: initial;
+  }
+
+  .progress-ring__label {
+    color: var(--syn-typography-color-text);
+    font: var(--syn-heading-x-large);
+  }
 `;

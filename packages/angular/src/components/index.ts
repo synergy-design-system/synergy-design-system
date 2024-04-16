3b9a65b4aba017c446c631db2a6962381b6d31c7
Christian Schilling <christian.schilling@sick.de>
2024-04-16 13:08:25 +0200

    Initial vendoring

--- packages/angular/src/components/index.ts
+++ packages/angular/src/components/index.ts
@@ -22,6 +22,7 @@ export { SynOptgroupComponent } from './optgroup.component';
 export { SynOptionComponent } from './option.component';
 export { SynPopupComponent } from './popup.component';
 export { SynPrioNavComponent } from './prio-nav.component';
+export { SynProgressRingComponent } from './progress-ring.component';
 export { SynRadioButtonComponent } from './radio-button.component';
 export { SynRadioComponent } from './radio.component';
 export { SynRadioGroupComponent } from './radio-group.component';

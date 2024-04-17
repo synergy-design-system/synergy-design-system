ce99a7a95180cc9c3449687e78799e9d051556cb
Christian Schilling <christian.schilling@sick.de>
2024-04-17 15:05:07 +0200

    Set correct default track color

--- packages/components/src/components/spinner/spinner.custom.styles.ts
+++ packages/components/src/components/spinner/spinner.custom.styles.ts
@@ -1,7 +1,11 @@
 import { css } from 'lit';
 
 export default css`
-.spinner__indicator {
-  stroke-linecap: initial;
-}
+  :host {
+    --track-color: var(--syn-color-neutral-200);
+  }
+
+  .spinner__indicator {
+    stroke-linecap: initial;
+  }
 `;

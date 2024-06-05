4835613018131bfa4042d387f902bdcac59aea13
Christian Schilling <christian.schilling@sick.de>
2024-06-05 15:42:42 +0200

    Added review suggestions

--- packages/_private/react-demo/src/Home.tsx
+++ packages/_private/react-demo/src/Home.tsx
@@ -2,10 +2,10 @@ import type { FC } from 'react';
 
 export const Home: FC = () => (
   <>
-    <h1 className="syn-heading-3x-large">
+    <h1 className="syn-heading--3x-large">
       Welcome to the Synergy React Demo!
     </h1>
-    <p className="syn-body-medium">
+    <p className="syn-body--medium">
       In this demo some use cases and our components are tested in the react ecosystem,
       like navigation handling, forms and theme switching.
     </p>
--- packages/_private/vue-demo/src/HomeView.vue
+++ packages/_private/vue-demo/src/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
-  <h1 class="syn-heading-3x-large">
+  <h1 class="syn-heading--3x-large">
     Welcome to the Synergy Vue Demo!
   </h1>
 
-  <p class="syn-body-medium">
+  <p class="syn-body--medium">
     In this demo some use cases and our components are tested in the Vue ecosystem,
     like navigation handling, forms and theme switching.
   </p>

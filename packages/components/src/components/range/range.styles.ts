3eb51791612136d12b9d9f749672e11faea3ffa7
Christian Schilling <christian.schilling@sick.de>
2024-07-01 10:35:04 +0200

    Initial copy of range slider and basic stories

--- packages/components/src/components/range/range.styles.ts
+++ packages/components/src/components/range/range.styles.ts
@@ -2,6 +2,131 @@ import { css } from 'lit';
 
 export default css`
   :host {
+    --thumb-size: 20px;
+    --tooltip-offset: 10px;
+    --track-color-active: var(--syn-color-primary-300);
+    --track-color-inactive: var(--syn-color-neutral-200);
+    --track-height: 6px;
+  }
+
+  .form-control {
+    align-items: stretch;
+    box-sizing: border-box;
+    display: flex;
+    flex-direction: column;
+    writing-mode: horizontal-tb;
+  }
+
+  .base {
     display: block;
+    height: var(--thumb-size);
+    position: relative;
+  }
+
+  :host([disabled]) .base {
+    opacity: 0.5;
+  }
+
+  .track {
+    background-color: var(--track-color-inactive);
+    border-radius: 3px;
+    display: inline-block;
+    height: var(--track-height);
+    margin: calc((var(--thumb-size) - var(--track-height)) / 2) calc(var(--thumb-size) / 2 - 3px);
+    width: calc(100% + 6px - var(--thumb-size));
+  }
+
+  .active-track {
+    background-color: var(--track-color-active);
+    height: var(--track-height);
+    position: absolute;
+    top: calc((var(--thumb-size) - var(--track-height)) / 2);
+    z-index: 2;
+  }
+
+  .handle {
+    background-color: var(--syn-color-primary-600);
+    border-radius: 50%;
+    cursor: pointer;
+    display: block;
+    height: var(--thumb-size);
+    position: absolute;
+    top: 0;
+    width: var(--thumb-size);
+    z-index: 3;
+  }
+
+  .handle:hover,
+  .handle.grabbed {
+    background-color: var(--syn-color-primary-500);
+  }
+
+  .handle.grabbed {
+    cursor: grabbing;
+  }
+
+  .handle:focus-visible,
+  .keyboard-focus .handle:focus {
+    outline: var(--syn-focus-ring);
+    outline-offset: var(--syn-focus-ring-offset);
+  }
+
+  :host([disabled]) .handle,
+  :host([disabled]) .handle.grabbed {
+    cursor: not-allowed;
+  }
+
+  .tooltip {
+    background-color: var(--syn-tooltip-background-color);
+    border-radius: var(--syn-tooltip-border-radius);
+    color: var(--syn-tooltip-color);
+    font-family: var(--syn-tooltip-font-family);
+    font-size: var(--syn-tooltip-font-size);
+    font-weight: var(--syn-tooltip-font-weight);
+    left: 0;
+    line-height: var(--syn-tooltip-line-height);
+    opacity: 0;
+    padding: var(--syn-tooltip-padding);
+    pointer-events: none;
+    position: absolute;
+    transition: var(--syn-transition-fast) opacity;
+    z-index: var(--syn-z-index-tooltip);
+  }
+
+  .tooltip::after {
+    content: '';
+    height: 0;
+    left: 50%;
+    position: absolute;
+    translate: calc(-1 * var(--syn-tooltip-arrow-size));
+    width: 0;
+  }
+
+  .tooltip-visible .tooltip {
+    opacity: 1;
+  }
+
+  /* Tooltip on top */
+  .tooltip-top .tooltip {
+    top: calc(-1 * var(--thumb-size) - var(--tooltip-offset));
+  }
+
+  .tooltip-top .tooltip::after {
+    border-left: var(--syn-tooltip-arrow-size) solid transparent;
+    border-right: var(--syn-tooltip-arrow-size) solid transparent;
+    border-top: var(--syn-tooltip-arrow-size) solid var(--syn-tooltip-background-color);
+    top: 100%;
+  }
+
+  /* Tooltip on bottom */
+  .tooltip-bottom .tooltip {
+    bottom: calc(-1 * var(--thumb-size) - var(--tooltip-offset));
+  }
+
+  .tooltip-bottom .tooltip::after {
+    border-bottom: var(--syn-tooltip-arrow-size) solid var(--syn-tooltip-background-color);
+    border-left: var(--syn-tooltip-arrow-size) solid transparent;
+    border-right: var(--syn-tooltip-arrow-size) solid transparent;
+    bottom: 100%;
   }
 `;
--- packages/docs/stories/components/range.stories.ts
+++ packages/docs/stories/components/range.stories.ts
@@ -2,7 +2,7 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable import/no-relative-packages */
 import type { Meta, StoryObj } from '@storybook/web-components';
-// import { html } from 'lit';
+import { html } from 'lit';
 import '../../../components/src/components/range/range.js';
 import {
   // generateScreenshotStory,
@@ -18,7 +18,8 @@ const { overrideArgs } = storybookHelpers('syn-range');
 const { generateTemplate } = storybookTemplate('syn-range');
 
 const meta: Meta = {
-  args: overrideArgs([], defaultArgs),
+  args: overrideArgs([
+  ], defaultArgs),
   argTypes,
   component: 'syn-range',
   parameters: {
@@ -48,3 +49,184 @@ export const Default: Story = {
   },
   render: (args: unknown) => generateTemplate({ args }),
 } as Story;
+
+export const Labels: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: generateStoryDescription('range', 'labels'),
+      },
+    },
+  },
+  render: () => html`
+    <syn-range label="Label"></syn-range>
+  `,
+};
+
+export const HelpText: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: generateStoryDescription('range', 'help-text'),
+      },
+    },
+  },
+  render: () => html`
+    <syn-range label="Volume" help-text="Controls the volume of the current song"></syn-range>
+  `,
+};
+
+export const Disabled: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: generateStoryDescription('range', 'disabled'),
+      },
+    },
+  },
+  render: () => html`
+    <syn-range disabled></syn-range>
+  `,
+};
+
+export const PrefixSuffixText: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: generateStoryDescription('range', 'prefix-suffix'),
+      },
+    },
+  },
+  render: () => html`
+    <syn-range label="Volume" help-text="Controls the volume of the current song">
+      <span slot="prefix">0</span>
+      <span slot="suffix">100</span>
+    </syn-range>
+
+    <p>This can be used to add input fields or icons.</p>
+
+    <syn-range label="Estimated Time">
+      <span slot="prefix">0</span>
+      <span slot="suffix">
+        60
+        <syn-input value="30">sec</syn-input>
+      </span>
+    </syn-range>
+  `,
+};
+
+export const CustomTrackColors: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: generateStoryDescription('range', 'custom-track-colors'),
+      },
+    },
+  },
+  render: () => html`
+    <syn-range class="custom-track-color"></syn-range>
+    <style>
+      .custom-track-color {
+        --track-color-active: var(--syn-color-success-600);
+      }
+    </style>
+  `,
+};
+
+export const CustomTrackOffset: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: generateStoryDescription('range', 'custom-track-offset'),
+      },
+    },
+  },
+  render: () => html`
+    <syn-range class="custom-track-offset"></syn-range>
+    <style>
+      .custom-track-offset {
+        --track-offset: 50px;
+      }
+    </style>
+  `,
+};
+
+export const MultiKnob: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: generateStoryDescription('range', 'multi-knob'),
+      },
+    },
+  },
+  render: () => html`
+    <syn-range value="20 80"></syn-range>
+  `,
+};
+
+export const Ticks: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: generateStoryDescription('range', 'ticks'),
+      },
+    },
+  },
+  render: () => html`
+    <syn-range>
+      <ol slot="ticks">
+        <li>0</li>
+        <li>25</li>
+        <li>50</li>
+        <li>75</li>
+        <li>100</li>
+      </ol>
+    </syn-range>
+  `,
+};
+
+export const TooltipPlacement: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: generateStoryDescription('range', 'tooltip-placement'),
+      },
+    },
+  },
+  render: () => html`
+    <syn-range tooltip="bottom"></syn-range>
+  `,
+};
+
+export const TooltipDisabled: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: generateStoryDescription('range', 'tooltip-disabled'),
+      },
+    },
+  },
+  render: () => html`
+    <syn-range tooltip="none"></syn-range>
+  `,
+};
+
+export const TooltipFormatter: Story = {
+  parameters: {
+    docs: {
+      description: {
+        story: generateStoryDescription('range', 'tooltip-formatter'),
+      },
+    },
+  },
+  render: () => html`
+    <syn-range class="tooltip-formatter"></syn-range>
+    <script>
+      document
+        .querySelectorAll('.tooltip-formatter')
+        .forEach(tip => {
+          tip.tooltipFormatter = value => value + '%';
+        });
+    </script>
+  `,
+};

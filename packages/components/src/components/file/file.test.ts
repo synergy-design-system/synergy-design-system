32860b071bc7232d4bb5c7c31fb342ed628c3d85
Christian Schilling <christian.schilling@sick.de>
2024-07-24 08:51:20 +0200

    Even more tests

--- packages/components/src/components/file/file.test.ts
+++ packages/components/src/components/file/file.test.ts
@@ -1,12 +1,12 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import sinon from 'sinon';
-import '../../../dist/synergy.js';
 import {
   expect,
   fixture,
   html,
   // waitUntil,
 } from '@open-wc/testing';
+import { serialize } from '../../../dist/synergy.js';
 import type SynFile from './file.js';
 import { runFormControlBaseTests } from '../../internal/test/form-control-base-tests.js';
 
@@ -74,6 +74,68 @@ describe('<syn-file>', () => {
     const uploadButton = el.shadowRoot!.querySelector('.input__button')!;
 
     expect(uploadButton).to.exist;
+  });
+
+  describe('when using constraint validation', () => {
+    it('should be valid by default', async () => {
+      const el = await fixture<SynFile>(html`<syn-file></syn-file>`);
+      expect(el.checkValidity()).to.be.true;
+    });
+
+    it('should be invalid when required and empty', async () => {
+      const el = await fixture<SynFile>(html`<syn-file required></syn-file>`);
+      expect(el.reportValidity()).to.be.false;
+      expect(el.checkValidity()).to.be.false;
+    });
+
+    it('should be invalid when required and disabled is removed', async () => {
+      const el = await fixture<SynFile>(html`<syn-file disabled required></syn-file>`);
+      el.disabled = false;
+      await el.updateComplete;
+      expect(el.checkValidity()).to.be.false;
+    });
+  });
+
+  describe('when submitting a form', () => {
+    it('should serialize its name and value with FormData', async () => {
+      const form = await fixture<HTMLFormElement>(html`<form><syn-file name="a"></syn-file></form>`);
+      const formData = new FormData(form);
+      expect(formData.get('a')).to.equal('');
+    });
+
+    it('should serialize its name and value with JSON', async () => {
+      const form = await fixture<HTMLFormElement>(html`<form><syn-file name="a"></syn-file></form>`);
+      const json = serialize(form) as { a: '' };
+      expect(json.a).to.equal('');
+    });
+
+    it('should be invalid when setCustomValidity() is called with a non-empty value', async () => {
+      const input = await fixture<HTMLFormElement>(html`<syn-file></syn-file>`);
+
+      (input as unknown as SynFile).setCustomValidity('Invalid selection');
+      await input.updateComplete;
+
+      expect(input.checkValidity()).to.be.false;
+      expect(input.hasAttribute('data-invalid')).to.be.true;
+      expect(input.hasAttribute('data-valid')).to.be.false;
+      expect(input.hasAttribute('data-user-invalid')).to.be.false;
+      expect(input.hasAttribute('data-user-valid')).to.be.false;
+    });
+
+    it('should be present in form data when using the form attribute and located outside of a <form>', async () => {
+      const el = await fixture<HTMLFormElement>(html`
+        <div>
+          <form id="f">
+            <syn-button type="submit">Submit</syn-button>
+          </form>
+          <syn-input form="f" name="a"></syn-input>
+        </div>
+      `);
+      const form = el.querySelector('form')!;
+      const formData = new FormData(form);
+
+      expect(formData.get('a')).to.equal('');
+    });
   });
 
   runFormControlBaseTests('syn-file');
--- packages/vue/LIMITATIONS.md
+++ packages/vue/LIMITATIONS.md
@@ -1,4 +1,4 @@
-# Known Issues and recipes - Vue
+# Known Issues and limitations - Vue
 
 > ⚠️ This file includes all known issues and limitations when using our Angular wrappers.
 > There may be other interesting bits of Information in our [components limitation documentation](https://synergy-design-system.github.io/?path=/docs/limitations-components--docs).

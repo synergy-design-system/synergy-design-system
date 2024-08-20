560bfce66d957dc600ab03e00ddadc9868378227
Susanne Kirchner <susanne.kirchner@sick.de>
2024-08-20 10:44:09 +0200

    Fix tests

--- packages/components/src/components/range-tick/range-tick.test.ts
+++ packages/components/src/components/range-tick/range-tick.test.ts
@@ -33,32 +33,4 @@ describe('<syn-range-tick>', () => {
       expect(tick).to.not.have.class('tick--subdivision');
     });
   });
-
-  describe('if no label is provided', () => {
-    it('should not add the className "tick--has-label" if there is no label provided via slot', async () => {
-      const el = await fixture<SynRangeTick>(html`<syn-range-tick></syn-range-tick>`);
-      const tick = el.shadowRoot?.querySelector('.tick');
-      expect(tick).to.not.have.class('tick--has-label');
-    });
-
-    it('should not add the className "tick--has-label" if there is no label provided via the label prop', async () => {
-      const el = await fixture<SynRangeTick>(html`<syn-range-tick label=""></syn-range-tick>`);
-      const tick = el.shadowRoot?.querySelector('.tick');
-      expect(tick).to.not.have.class('tick--has-label');
-    });
-  });
-
-  describe('if a label is provided', () => {
-    it('should add the className "tick--has-label" if it is provided via the default slot', async () => {
-      const el = await fixture<SynRangeTick>(html`<syn-range-tick>Tick Label</syn-range-tick>`);
-      const tick = el.shadowRoot?.querySelector('.tick');
-      expect(tick).to.have.class('tick--has-label');
-    });
-
-    it('should add the className "tick--has-label" if it is provided via the label prop prop', async () => {
-      const el = await fixture<SynRangeTick>(html`<syn-range-tick label="Tick Label"></syn-range-tick>`);
-      const tick = el.shadowRoot?.querySelector('.tick');
-      expect(tick).to.have.class('tick--has-label');
-    });
-  });
 });

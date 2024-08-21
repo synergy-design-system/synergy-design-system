a74fb74fa413884d2cfbf4cd9b289a08a6390e86
Christian Schilling <christian.schilling@sick.de>
2024-08-21 14:29:47 +0200

    Adjustments from review

--- packages/docs/stories/styles/link.stories.ts
+++ packages/docs/stories/styles/link.stories.ts
@@ -48,7 +48,60 @@ export const Default: StoryObj = {
       disable: false,
     },
   },
-  render: (args: unknown) => renderStyles(args as RenderArgs, 'a'),
+  render: (args: unknown) => renderStyles(args as RenderArgs, 'a', {
+    class: 'syn-link',
+    // eslint-disable-next-line no-script-url
+    href: 'javascript:void(0)',
+  }),
+};
+
+export const Inline: StoryObj = {
+  parameters: {
+    docs: {
+      description: {
+        story: generateStoryDescription('link', 'inline'),
+      },
+    },
+  },
+  render: () => html`
+    <p style="width: 266px;">
+      This is some random text that contains <a href="javascript:void(0)" class="syn-link">a link that spans over several lines</a> to imply how it works in real life.
+    </p>
+  `,
+};
+
+export const PrefixSuffixIcons: StoryObj = {
+  parameters: {
+    docs: {
+      description: {
+        story: generateStoryDescription('link', 'prefix-suffix-icons'),
+      },
+    },
+  },
+  render: () => html`
+    <nav class="storybook-demo-syn-link">
+      <a href="javascript:void(0)" class="syn-link syn-link--medium">
+        Open link in new window
+        <syn-icon name="arrow_outward"></syn-icon>
+      </a>
+      <a href="javascript:void(0)" class="syn-link syn-link--medium">
+        <syn-icon name="picture_as_pdf"></syn-icon>
+        Download PDF
+      </a>
+      <a href="javascript:void(0)" class="syn-link syn-link--medium">
+        <syn-icon name="picture_as_pdf"></syn-icon>
+        Open PDF in new window
+        <syn-icon name="arrow_outward"></syn-icon>
+      </a>
+    </nav>
+    <style>
+      .storybook-demo-syn-link {
+        display: inline-flex;
+        flex-direction: column;
+        gap: var(--syn-spacing-large);
+      }
+    </style>
+  `,
 };
 
 export const Quiet: StoryObj = {
@@ -76,7 +129,7 @@ export const Disabled: StoryObj = {
     },
   },
   render: () => html`
-    <a href="javascript:alert('Here')" class="syn-link syn-link--disabled">
+    <a class="syn-link syn-link--disabled">
       <syn-icon name="keyboard_arrow_right"></syn-icon>
       Link Label
     </a>
@@ -108,23 +161,6 @@ export const Focus: StoryObj = {
   `,
 };
 
-export const PrefixSuffixIcons: StoryObj = {
-  parameters: {
-    docs: {
-      description: {
-        story: generateStoryDescription('link', 'prefix-suffix-icons'),
-      },
-    },
-  },
-  render: () => html`
-    <a href="javascript:void(0)" class="syn-link">
-      <syn-icon name="arrow_outward"></syn-icon>
-      Link Label
-      <syn-icon name="refresh"></syn-icon>
-    </a>
-  `,
-};
-
 export const Size: StoryObj = {
   parameters: {
     docs: {
@@ -151,30 +187,14 @@ export const Size: StoryObj = {
   `,
 };
 
-export const Inline: StoryObj = {
-  parameters: {
-    docs: {
-      description: {
-        story: generateStoryDescription('link', 'inline'),
-      },
-    },
-  },
-  render: () => html`
-    <p style="width: 220px;">
-      Qui do.
-      <a href="javascript:void(0)" class="syn-link">Magna ex elit cupidatat non esse</a>.
-      Eiusmod minim excepteur.
-    </p>
-  `,
-};
-
 /* eslint-disable sort-keys */
 export const Screenshot: StoryObj = generateScreenshotStory({
   Default,
+  Inline,
+  PrefixSuffixIcons,
   Quiet,
   Disabled,
-  PrefixSuffixIcons,
+  Focus,
   Size,
-  Inline,
 });
 /* eslint-enable sort-keys */

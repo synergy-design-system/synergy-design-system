9ef426301f137d9f35695dba278bc20a6a15d2b1
Susanne Kirchner <susanne.kirchner@sick.de>
2024-09-18 12:56:47 +0200

    Add review findings

--- packages/components/src/components/combobox/option-renderer.ts
+++ packages/components/src/components/combobox/option-renderer.ts
@@ -16,27 +16,28 @@ export type OptionRenderer = (
 /**
  * The default option renderer, which does not change the option.
  */
-export const defaultOptionRenderer = (option: SynOption) => option;
+export const defaultOptionRenderer: OptionRenderer = (option: SynOption) => option;
 
 /**
  * A function that highlights the query string with a mark element in the option.
  */
-export const highlightOptionRenderer = (option: SynOption, query: string) => {
-  if (query) {
-    const clonedOption = option.cloneNode(true) as SynOption;
-    const optionLabel = clonedOption.getTextLabel();
-    const queryIndex = optionLabel.toLowerCase().indexOf(query.toLowerCase());
-    const indexLabel = clonedOption.innerHTML.indexOf(optionLabel);
-
-    const mark = document.createElement('mark');
-    mark.textContent = optionLabel.slice(queryIndex, queryIndex + query.length);
-
-    const exchangedText = optionLabel.replace(new RegExp(query, 'i'), mark.outerHTML);
-    const previousContent = clonedOption.innerHTML.slice(0, indexLabel);
-    const followingContent = clonedOption.innerHTML.slice(indexLabel + optionLabel.length);
-    // eslint-disable-next-line no-param-reassign
-    clonedOption.innerHTML = previousContent.concat(exchangedText, followingContent);
-    return clonedOption;
+export const highlightOptionRenderer: OptionRenderer = (option: SynOption, query: string) => {
+  if (!query) {
+    return option;
   }
-  return option;
+
+  const clonedOption = option.cloneNode(true) as SynOption;
+  const optionLabel = clonedOption.getTextLabel();
+  const queryIndex = optionLabel.toLowerCase().indexOf(query.toLowerCase());
+  const indexLabel = clonedOption.innerHTML.indexOf(optionLabel);
+
+  const mark = document.createElement('mark');
+  mark.textContent = optionLabel.slice(queryIndex, queryIndex + query.length);
+
+  const exchangedText = optionLabel.replace(new RegExp(query, 'i'), mark.outerHTML);
+  const previousContent = clonedOption.innerHTML.slice(0, indexLabel);
+  const followingContent = clonedOption.innerHTML.slice(indexLabel + optionLabel.length);
+  // eslint-disable-next-line no-param-reassign
+  clonedOption.innerHTML = previousContent.concat(exchangedText, followingContent);
+  return clonedOption;
 };

c39c1d44e76b0881cf36ad4733a55f3ebcbdafe7
Susanne Kirchner <susanne.kirchner@sick.de>
2024-09-20 16:51:45 +0200

    Add correct styling to footer template

--- packages/docs/stories/templates/footer.stories.tsx
+++ packages/docs/stories/templates/footer.stories.tsx
@@ -8,7 +8,7 @@ import {
 } from '@storybook/blocks';
 import { html } from 'lit';
 import storyBookPreviewConfig from '../../.storybook/preview.js';
-// import { generateStoryDescription } from '../../src/helpers/component.js';
+import { generateStoryDescription } from '../../src/helpers/component.js';
 import { getTranslation } from '../../src/translations.js';
 import { generateFigmaPluginObject } from '../../src/helpers/figma.js';
 
@@ -17,8 +17,7 @@ const meta: Meta = {
     design: generateFigmaPluginObject('25171-45729'),
     docs: {
       description: {
-        // TODO: add description as soon as docs tokens are available
-        // component: generateStoryDescription('templates', 'footer'),
+        component: generateStoryDescription('templates', 'footer'),
       },
       page: () => (
         <>
@@ -29,7 +28,6 @@ const meta: Meta = {
         </>
       ),
       story: {
-        // iframeHeight: 1600,
         inline: true,
       },
     },
@@ -48,10 +46,47 @@ export const Footer = {
   },
   render: () => html`
     <style>
+        .synergy-footer-demo {
+          align-items: center;
+          display: flex;
+          gap: var(--syn-spacing-x-large);
+          justify-content: space-between;
+          padding: var(--syn-spacing-large) var(--syn-spacing-2x-large);
+        }
+
+        .synergy-footer-demo .copyright {
+          color: var(--syn-typography-color-text);
+          font: var(--syn-body-small-bold);
+          margin: 0;
+        }
+
+        .synergy-footer-demo .link-wrapper {
+          display: flex;
+          gap: var(--syn-spacing-large);
+          justify-content: center;
+          list-style: none;
+          margin: 0;
+          padding: 0;
+        }
+
+        /* TODO: may do something with container query. I tried it, but together with display: flex some strange things happened */
+        @media only screen and (max-width: 640px) {
+          .link-wrapper {
+            flex-direction: column;
+            gap: var(--syn-spacing-small);
+          }
+
+          .synergy-footer-demo {
+            align-items: flex-start;
+            flex-direction: column;
+            padding: var(--syn-spacing-large) var(--syn-spacing-medium);
+          }
+        }
+
     </style>
     <footer class="synergy-footer-demo">
       <nav>
-        <ul>
+        <ul class="link-wrapper">
           <li>
             <a class="syn-link syn-link--medium syn-link--quiet" href="${getTranslation('footer.linksHref.imprint')}">${getTranslation('footer.linksName.imprint')}</a>
           </li>
@@ -66,16 +101,12 @@ export const Footer = {
           </li>
         </ul>
       </nav>
-      <p>&copy; 2014 SpaceBears Inc</p>
+      <p class="copyright">&copy; 2024 SICK AG</p>
     </footer>
   `,
 };
-// hreflang??
-// footer --> role="contentinfo"
-// nav --> role="navigation"
-// nav with role="navigation"?
-// aria-label on footer?
 
+// TODO: I don't know why, but for some reason it does not switch the viewport to mobile2
 export const FooterTablet = {
   ...Footer,
   name: '↳ Tablet',

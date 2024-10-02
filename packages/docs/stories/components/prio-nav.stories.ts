0d2ea2a1ed146ce431ee26e33f9adc35ca19ab2c
Christian Schilling <christian.schilling@sick.de>
2024-10-02 10:41:45 +0200

    Fix for #631

--- packages/docs/stories/components/prio-nav.stories.ts
+++ packages/docs/stories/components/prio-nav.stories.ts
@@ -26,7 +26,7 @@ const meta: Meta = {
       value: `
         <syn-nav-item current horizontal>Domains</syn-nav-item>
         <syn-nav-item horizontal>Projects</syn-nav-item>
-        <syn-nav-item horizontal>Trainings</syn-nav-item>
+        <syn-nav-item horizontal href="javascript:void(0)">Trainings</syn-nav-item>
       `,
     },
   ], defaultArgs),

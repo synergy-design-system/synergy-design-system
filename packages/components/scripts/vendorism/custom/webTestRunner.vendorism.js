88c30c0dd4fdbee45714b9c17dc88bb049b53197
Susanne Kirchner <susanne.kirchner@sick.de>
2024-12-13 11:45:37 +0100

    Make testing fail if "only" is used in a test

--- packages/components/scripts/vendorism/custom/webTestRunner.vendorism.js
+++ packages/components/scripts/vendorism/custom/webTestRunner.vendorism.js
@@ -1,3 +1,5 @@
+import { addSectionsAfter, replaceSections } from '../replace-section.js';
+
 /**
  * Adjust the web-test-runner config
  */
@@ -9,31 +11,37 @@ export const vendorWebTestRunnerConfig = (path, content) => {
     };
   }
 
-  // Adjust the path to the theme to make sure we always fetch
-  // the latest version from the package
-  let nextContent = content.replaceAll(
-    '<link rel="stylesheet" href="dist/themes/light.css">',
-    `<link rel="stylesheet" href="node_modules/@synergy-design-system/tokens/dist/themes/light.css">
-        <link rel="stylesheet" href="dist/styles/index.css">`,
-  );
+  let nextContent = addSectionsAfter([
+    // Add the synergy test plugins to the imports
+    [
+      "runner-playwright';",
+      "import synTestPlugins from './scripts/tests/index.js';",
+    ],
+    // Add the synergy test plugins to the plugins
+    [
+      'plugins: [',
+      '...synTestPlugins.plugins,',
+      { tabsBeforeInsertion: 2 },
+    ],
+    // Adjust the path to the theme to make sure we always fetch the latest version from the package
+    [
+      '<link rel="stylesheet" href="dist/themes/light.css">',
+      '<link rel="stylesheet" href="node_modules/@synergy-design-system/tokens/dist/themes/light.css">',
+      { tabsBeforeInsertion: 4 },
+    ],
+    // add mocha config to fail if somewhere is `.only` used in a test
+    [
+      'timeout: 3000,',
+      'forbidOnly: true,',
+      { tabsBeforeInsertion: 3 },
+    ],
+  ], content);
 
-  // Add the synergy test plugins to the mix
-  nextContent = nextContent.replaceAll(
-    "runner-playwright';",
-    `runner-playwright';
-import synTestPlugins from './scripts/tests/index.js';`,
-  );
-
-  nextContent = nextContent.replaceAll(
-    'esbuildPlugin({',
-    `...synTestPlugins.plugins,
-    esbuildPlugin({`,
-  );
-
-  // Enable testing with firefox.
-  // TODO: As soon as shoelace enabled it on their side, this can be removed
-  // TODO: We add concurrency: 1 to prevent the issue at https://github.com/modernweb-dev/web/issues/2374
-  nextContent = nextContent.replace(
+  nextContent = replaceSections([
+    // Enable testing with firefox.
+    // TODO: As soon as shoelace enabled it on their side, this can be removed
+    // TODO: We add concurrency: 1 to prevent the issue at https://github.com/modernweb-dev/web/issues/2374
+    [
       `// Firefox started failing randomly so we're temporarily disabling it here. This could be a rogue test, not really
     // sure what's happening.
     // playwrightLauncher({ product: 'firefox' }),`,
@@ -42,7 +50,11 @@ import synTestPlugins from './scripts/tests/index.js';`,
     // @see https://github.com/modernweb-dev/web/issues/2374
     playwrightLauncher({ product: 'firefox', concurrency: 1 }),
 `,
-  );
+    ],
+  ], nextContent);
+
+  // add mocha config to fail if somewhere is `.only` used in a test
+  // );
   return {
     content: nextContent,
     path,

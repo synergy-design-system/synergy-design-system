e5a5b5260b79cc26a35e67020dbc647f24e161d7
Christian Schilling <christian.schilling@sick.de>
2025-01-03 12:03:04 +0100

    React set default on load

--- packages/_private/react-demo/src/App.tsx
+++ packages/_private/react-demo/src/App.tsx
@@ -1,17 +1,26 @@
+import { useEffect } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout';
 import { DemoForm } from './DemoForm';
 import { DemoFormValidate } from './DemoFormValidate';
 import { Home } from './Home';
+import { setGlobalSize } from './shared';
 
-export const App = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="contact-form" element={<DemoForm />} />
-        <Route path="contact-form-validate" element={<DemoFormValidate />} />
-      </Route>
-    </Routes>
-  </BrowserRouter>
-);
+export const App = () => {
+  // Use this to set the global size to medium
+  // for the whole application
+  useEffect(() => {
+    setGlobalSize('medium');
+  }, []);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="contact-form" element={<DemoForm />} />
+          <Route path="contact-form-validate" element={<DemoFormValidate />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+};

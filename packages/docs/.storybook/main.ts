7780a4d51013a3ed61de425df8d1053a473ee26f
Christian Schilling <christian.schilling@sick.de>
2025-01-20 12:09:51 +0100

    Review: Moved global settings doc to components namespace

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -37,7 +37,6 @@ const config: StorybookConfig = {
     "../stories/Contributing.mdx",
     "../stories/Accessibility.mdx",
     "../stories/IconSearch.mdx",
-    "../stories/GlobalSettings.mdx",
     "../stories/limitations/**/*.mdx",
     "../stories/packages/**/*.mdx",
     "../stories/tokens/**/*.@(mdx|stories.*)",

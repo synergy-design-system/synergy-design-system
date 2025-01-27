3fa43f2da5bcb1504ccfe9ab6fb9212763b2fa89
Christian Schilling <christian.schilling@sick.de>
2025-01-27 08:30:57 +0100

    Export only external interface in index.js

--- packages/components/src/utilities/defaultSettings/index.ts
+++ packages/components/src/utilities/defaultSettings/index.ts
@@ -1,4 +1,10 @@
 export type * from './base.js';
 export { defaultSettings, INITIAL_DEFAULT_SETTINGS } from './base.js';
 export * from './decorator.js';
-export * from './functions.js';
+
+// We only export the outbound interface of the decorator
+export {
+  enableExperimentalSettingEmitEvents,
+  setDefaultSettingsForElement,
+  setGlobalDefaultSettings,
+} from './functions.js';

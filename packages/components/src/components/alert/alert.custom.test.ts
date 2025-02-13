75ce3d768f856fa6e71b3e2ab68b12b4a1d3e238
Susanne Kirchner <susanne.kirchner@sick.de>
2025-02-13 11:20:37 +0100

    Fix lint errors

--- packages/components/src/components/alert/alert.custom.test.ts
+++ packages/components/src/components/alert/alert.custom.test.ts
@@ -1,16 +1,14 @@
-import '../../../dist/synergy.js';
-import { expect, fixture, html } from '@open-wc/testing';
-import { clickOnElement } from '../../internal/test.js';
-import { resetMouse } from '@web/test-runner-commands';
 import sinon from 'sinon';
+import { expect, fixture, html } from '@open-wc/testing';
+import { resetMouse } from '@web/test-runner-commands';
+import '../../../dist/synergy.js';
+import { clickOnElement } from '../../internal/test.js';
 import type SynAlert from './alert.js';
 import type SynIconButton from '../icon-button/icon-button.js';
 
-const getCloseButton = (alert: SynAlert): SynIconButton | null | undefined =>
-  alert.shadowRoot?.querySelector<SynIconButton>('[part="close-button"]');
+const getCloseButton = (alert: SynAlert): SynIconButton | null | undefined => alert.shadowRoot?.querySelector<SynIconButton>('[part="close-button"]');
 
 describe('<syn-alert>', () => {
-
   afterEach(async () => {
     await resetMouse();
   });
@@ -20,7 +18,7 @@ describe('<syn-alert>', () => {
       const wrapper = await fixture<HTMLDivElement>(html`<div class="wrapper" style="padding: 24px; background-color:red;"><syn-alert open closable>I am an alert</syn-alert></div>`);
       const alert = wrapper.querySelector('syn-alert') as SynAlert;
 
-      let clickTargetPromise = new Promise<HTMLElement>((resolve) => {
+      const clickTargetPromise = new Promise<HTMLElement>((resolve) => {
         const clickHandler = sinon.spy((event: MouseEvent) => {
           resolve(event.target as HTMLElement);
         });
@@ -28,12 +26,11 @@ describe('<syn-alert>', () => {
         wrapper.addEventListener('click', clickHandler);
       });
 
-
       const closeButton = getCloseButton(alert);
-      clickOnElement(closeButton!, 'top', 0, -4);
+      await clickOnElement(closeButton!, 'top', 0, -4);
       const clickTarget = await clickTargetPromise;
-
-      expect(clickTarget.tagName.toLowerCase()).to.not.be.equal('syn-icon-button');
+      const tag = clickTarget.tagName.toLowerCase();
+      await expect(tag).to.not.be.equal('syn-icon-button');
       expect(clickTarget.classList.contains('alert')).to.be.true;
       expect(clickTarget.classList.contains('wrapper'), 'The click should happen in the alert and not outside of it').to.be.false;
     });
@@ -42,7 +39,7 @@ describe('<syn-alert>', () => {
       const wrapper = await fixture<HTMLDivElement>(html`<div class="wrapper" style="padding: 24px; background-color:red;"><syn-alert open closable>I am an alert</syn-alert></div>`);
       const alert = wrapper.querySelector('syn-alert') as SynAlert;
 
-      let clickTargetPromise = new Promise<HTMLElement>((resolve) => {
+      const clickTargetPromise = new Promise<HTMLElement>((resolve) => {
         const clickHandler = sinon.spy((event: MouseEvent) => {
           resolve(event.target as HTMLElement);
         });
@@ -50,12 +47,11 @@ describe('<syn-alert>', () => {
         wrapper.addEventListener('click', clickHandler);
       });
 
-
       const closeButton = getCloseButton(alert);
-      clickOnElement(closeButton!, 'bottom', 0, 4);
+      await clickOnElement(closeButton!, 'bottom', 0, 4);
       const clickTarget = await clickTargetPromise;
 
-      expect(clickTarget.tagName.toLowerCase()).to.not.be.equal('syn-icon-button');
+      await expect(clickTarget.tagName.toLowerCase()).to.not.be.equal('syn-icon-button');
       expect(clickTarget.classList.contains('alert')).to.be.true;
       expect(clickTarget.classList.contains('wrapper'), 'The click should happen in the alert and not outside of it').to.be.false;
     });

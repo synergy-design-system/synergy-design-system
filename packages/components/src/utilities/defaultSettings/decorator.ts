46b3ebd5f39f58682995d75e3b665dfd6b800961
Susanne Kirchner <susanne.kirchner@sick.de>
2025-03-11 11:37:27 +0100

    fix: 🐛 setGlobalDefaultSettings does overwrite explicitly set default property values with new default size (#801)

--- packages/components/src/utilities/defaultSettings/decorator.ts
+++ packages/components/src/utilities/defaultSettings/decorator.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-underscore-dangle */
-import type { PropertyValues } from 'lit';
+import type { PropertyDeclaration, PropertyValues } from 'lit';
 import type SynergyElement from '../../internal/synergy-element.js';
 import { type ComponentNamesWithDefaultValues } from './base.js';
 import {
@@ -25,9 +25,23 @@ export function enableDefaultSettings(name: ComponentNamesWithDefaultValues) {
 
     #initialGlobalSettingEmptyProperties = new Map<string, unknown>();
 
+    #initialProperties: Array<string> = [];
+
+    #systemDefaultSettings: Record<string, unknown>;
+
+    private _isInitialized = false;
+
     // eslint-disable-next-line class-methods-use-this
     get __originalDecoratedClassName() {
       return name;
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    constructor(...args: any[]) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+      super(...args);
+      this._isInitialized = true;
+      this.#systemDefaultSettings = extractDefaultSettingsForElement(name, 'initial');
     }
 
     /**
@@ -52,6 +66,29 @@ export function enableDefaultSettings(name: ComponentNamesWithDefaultValues) {
       removeGlobalEventNotification(this);
     }
 
+    // eslint-disable-next-line complexity
+    requestUpdate(propName?: PropertyKey, oldValue?: unknown, options?: PropertyDeclaration) {
+      super.requestUpdate(propName, oldValue, options);
+
+      if (
+        // element initialization is not ready
+        !this._isInitialized
+        // setup is already complete
+        || this.#globalSettingsSetupComplete
+        || !propName
+        // skip properties, which do not belong to global settings
+        || !(propName in this.#systemDefaultSettings)
+        // skip properties, which are already in the initial properties
+        || this.#initialProperties?.includes(propName as string)) {
+        return;
+      }
+
+      // Only explicit set properties / attributes on the element will trigger a requestUpdate
+      // (e.g. <syn-button size="medium" /> ). If the default is used (e.g. <syn-button />),
+      // the property will not appear in the _initialProperties array.
+      this.#initialProperties.push(propName as string);
+    }
+
     protected willUpdate(changedProps: PropertyValues): void {
       super.willUpdate(changedProps);
 
@@ -65,20 +102,20 @@ export function enableDefaultSettings(name: ComponentNamesWithDefaultValues) {
       // Get the default settings
       const defaults = extractDefaultSettingsForElement(name);
 
-      const systemDefaults = extractDefaultSettingsForElement(name, 'initial');
-
       // Set the default values for all items that have no current value set
       Object
         .entries(defaults)
         .forEach(([key, value]) => {
           const currentProp = this[key as keyof this];
-          const originalDefaultSetting = systemDefaults[key];
+          const originalDefaultSetting = this.#systemDefaultSettings[key];
 
           // On initial load, the attribute is not set, but the property is.
           // We have to check if the current PROPERTY is the same as the default value
           // If it is, we set the attribute to the default value and add a notification item
           // to the initialGlobalSettingEmptyProperties map
-          if (currentProp === originalDefaultSetting) {
+          // We also need to check if the default value of the property was used
+          // or the property was explicitly set on the element
+          if (currentProp === originalDefaultSetting && !this.#initialProperties.includes(key)) {
             this.#initialGlobalSettingEmptyProperties.set(key, currentProp);
             // @ts-expect-error We don´t know the type of the key,
             // but are pretty sure it exists on the element

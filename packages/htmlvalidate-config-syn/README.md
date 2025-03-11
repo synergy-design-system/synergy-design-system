ed15ce4618e37f6b7e425653355ccd5ae1f0b38e
Christian Schilling <christian.schilling@sick.de>
2025-03-11 13:38:08 +0100

    Added dev documentation

--- packages/htmlvalidate-config-syn/README.md
+++ packages/htmlvalidate-config-syn/README.md
@@ -42,3 +42,55 @@ export default defineConfig({
   root: true,
 });
 ```
+
+---
+
+## Development
+
+Provided rules are created dynamically with some static overrides that cannot be obtained from the provided components manifest.
+Each element has its own custom configuration that overrides all dynamically created settings.
+Custom rules for each element can be found in `scripts/rules.js`.
+Most of those rules adjust the [Content category](https://html-validate.org/usage/elements.html#content-categories) used for the element. It also keeps track for various other actions like flagging a custom element as [`formAssociated`](https://html-validate.org/usage/elements.html#formassociated).
+
+### Deprecating properties for future releases
+
+For future releases where an attribute is phased out, it is desirable to flag the attribute as [`deprecated`](https://html-validate.org/usage/elements.html#deprecated). This can be done in the custom configuration like this:
+
+```javascript
+// Example: <syn-header>
+// Synergy@1.x still has the attribute show-burger-menu for syn-header,
+// but we know we want to remove it in version 2.0
+/**
+ * @type {import('html-validate').MetaElement}
+ */
+const SynHeader = {
+  attributes: {
+    'show-burger-menu': {
+      boolean: true,
+      deprecated: willDeprecateInRelease('2.0', 'Will be replaced with `burger-menu`'),
+    },
+  },
+  flow: true,
+  heading: true,
+  sectioning: true,
+};
+```
+
+When the new major version is released, you may finally deprecate the property:
+
+```javascript
+// Example: <syn-header>
+// Synergy@2.x removed the attribute show-burger-menu for syn-header,
+// so we replace the warning with a hard error
+const SynHeader = {
+  attributes: {
+    'show-burger-menu': {
+      allowed: deprecatedForRelease('2.0', 'Please use the `burger-menu` attribute instead.'),
+      boolean: true,
+    },
+  },
+  flow: true,
+  heading: true,
+  sectioning: true,
+};
+```
--- packages/htmlvalidate-config-syn/scripts/rules.js
+++ packages/htmlvalidate-config-syn/scripts/rules.js
@@ -7,11 +7,23 @@ const metadata = JSON.parse(fs.readFileSync(metadataPath, 'utf-8'));
 
 /**
  * Creates a deprecation message for a given synergy version
+ * @param {string} version The Synergy Version the attribute will be deprecated in
+ * @param {string} reason [optional]
+ * @returns {string} The final deprecation message
+ */
+// eslint-disable-next-line no-unused-vars
+const willDeprecateInRelease = (version, reason = '') => `
+  This attribute is deprecated and will be removed in Synergy version ${version}!
+  ${reason}
+`.trim();
+
+/**
+ * Creates a deprecation message for a given synergy version
  * @param {string} version The Synergy Version the attribute was deprecated in
  * @param {string} reason [optional]
  * @returns {string} The final deprecation message
  */
-const deprecateIn = (version, reason = '') => `
+const deprecatedForRelease = (version, reason = '') => `
 This attribute was deprecated in Synergy version ${version}!
 ${reason}
 Further information can be found at https://github.com/synergy-design-system/synergy-design-system/blob/main/packages/components/BREAKING_CHANGES.md#version-${version.replaceAll('.', '')} for further information.
@@ -21,7 +33,7 @@ Further information can be found at https://github.com/synergy-design-system/syn
 // Please add one deprecation warning per major version.
 // Also note that you have to return a function that calls
 // deprecateIn, otherwise the message will not be displayed.
-const deprecatedInSyn2 = (reason = '') => () => deprecateIn('2.0', reason);
+const deprecatedForV2 = (reason = '') => () => deprecatedForRelease('2.0', reason);
 
 // Shared settings for form associated elements
 const formAssociated = {
@@ -99,7 +111,7 @@ const SynButtonGroup = {
 const SynCard = {
   attributes: {
     nested: {
-      allowed: deprecatedInSyn2('Please use the `sharp` property instead.'),
+      allowed: deprecatedForV2('Please use the `sharp` property instead.'),
       boolean: true,
     },
   },
@@ -185,7 +197,7 @@ const SynFile = {
 const SynHeader = {
   attributes: {
     'show-burger-menu': {
-      allowed: deprecatedInSyn2('Please use the `burger-menu` attribute instead.'),
+      allowed: deprecatedForV2('Please use the `burger-menu` attribute instead.'),
       boolean: true,
     },
   },

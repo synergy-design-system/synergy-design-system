ba7b32dbeb24d204d5fcad26479c3c54e274d716
Christian Schilling <christian.schilling@sick.de>
2025-05-26 10:59:02 +0200

    Rename modes as they dont align with chromatic name strategy

--- packages/docs/.storybook/modes.ts
+++ packages/docs/.storybook/modes.ts
@@ -1,7 +1,7 @@
 // Make sure chromatic supports dark mode
 // @see https://www.chromatic.com/docs/themes/
-export const SICK_2025_LIGHT = 'SICK 2025 Light (v3)';
-export const SICK_2025_DARK = 'SICK 2025 Dark (v3)';
+export const SICK_2025_LIGHT = '🌞 SICK 2025 Light (v3)';
+export const SICK_2025_DARK = '🌙 SICK 2025 Dark (v3)';
 
-export const SICK_2018_LIGHT = 'SICK 2018 Light (v2)';
-export const SICK_2018_DARK = 'SICK 2018 Dark (v2)';
+export const SICK_2018_LIGHT = '🌞 light';
+export const SICK_2018_DARK = '🌙 dark';

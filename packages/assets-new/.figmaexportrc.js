4ac8b9ba848bac0f2c4fc4c0e4b28a57ca312f40
Christian Schilling <christian.schilling@sick.de>
2025-06-02 10:32:28 +0200

    Clean up configuration

--- packages/assets-new/.figmaexportrc.js
+++ packages/assets-new/.figmaexportrc.js
@@ -1,102 +1,12 @@
-import outputComponentsAsSvg from '@figma-export/output-components-as-svg'
-import transformSvgWithSvgo from '@figma-export/transform-svg-with-svgo'
+import {
+  DEFAULT_CONFIG,
+} from './scripts/figma-config.js';
 
-/** @type { import('@figma-export/types').ComponentsCommandOptions } */
-const systemIconConfig = {
-  fileId: 'bZFqk9urD3NlghGUKrkKCR',
-  ids: [
-    '1616-1509', // System Icons
-    // '41-4000', // logos
-    // '1616-1512', // Icons
-  ],
-  onlyFromPages: ['Assets'],
-  includeTypes: ['COMPONENT', 'VARIANT'],
-  transformers: [
-    transformSvgWithSvgo({
-      plugins: [
-            {
-              name: 'removeAttrs',
-              params: { attrs: 'fill' },
-            },
-            {
-              name: 'addAttributesToSVGElement',
-              params: {
-                attributes: ["fill='currentColor'"],
-              },
-            },
-          ]
-    })
-  ],
-  outputters: [
-    outputComponentsAsSvg({
-      getDirname: () => `system-icons`,
-      output: './src'
-    })
-  ]
-};
-
-const iconConfig = {
-  fileId: 'bZFqk9urD3NlghGUKrkKCR',
-  ids: [
-    // '1616-1509', // System Icons
-    // '41-4000', // logos
-    '1616-1512', // Icons
-  ],
-  onlyFromPages: ['Assets'],
-  includeTypes: ['COMPONENT', 'VARIANT'],
-  transformers: [
-    transformSvgWithSvgo({
-      plugins: [
-            {
-              name: 'removeAttrs',
-              params: { attrs: 'fill' },
-            },
-            {
-              name: 'addAttributesToSVGElement',
-              params: {
-                attributes: ["fill='currentColor'"],
-              },
-            },
-          ]
-    })
-  ],
-  outputters: [
-    outputComponentsAsSvg({
-      getDirname: () => `icons`,
-      output: './src'
-    })
-  ]
-};
-
-const logoConfig = {
-  fileId: 'bZFqk9urD3NlghGUKrkKCR',
-  ids: [
-    // '1616-1509', // System Icons
-    '41-4000', // logos
-    // '1616-1512', // Icons
-  ],
-  onlyFromPages: ['Assets'],
-  includeTypes: ['COMPONENT', 'VARIANT'],
-  transformers: [
-    transformSvgWithSvgo({
-      plugins: []
-    }),
-  ],
-  outputters: [
-    outputComponentsAsSvg({
-      getBasename: ({ basename }) => {
-        return basename.replace('name=', '') + '.svg';
-      },
-      getDirname: () => `logos`,
-      output: './src'
-    })
-  ]
-};
+const commands = DEFAULT_CONFIG.map(config => [
+  'components',
+  config,
+]);
 
 export default {
-  commands: [
-    ['components', systemIconConfig],
-    ['components', iconConfig],
-    ['components', logoConfig],
-  ]
-}
+  commands,
+};
---
+++ packages/assets-new/scripts/config.js
@@ -0,0 +1,17 @@
+// V2 Figma paths
+export const FIGMA_FILE_ID_ICONS_V2 = 'bZFqk9urD3NlghGUKrkKCR';
+export const FIGMA_ID_SYSTEM_ICONS_V2 = '1616-1509';
+export const FIGMA_ID_ALL_ICONS_V2 = '1616-1512';
+export const FIGMA_ID_LOGOS_V2 = '41-4000';
+
+// V3 Figma paths
+export const FIGMA_FILE_ID_ICONS_V3 = 'ltRW0fLoFIuHyuh73DsjrZ';
+export const FIGMA_ID_FILLED_ICONS_V3 = '4-713';
+export const FIGMA_ID_OUTLINE_ICONS_V3 = '4-731';
+
+// Paths
+export const PATH_ICONS_V2 = 'src/icons';
+export const PATH_LOGOS_V2 = 'src/logos';
+export const PATH_SYSTEM_ICONS_V2 = 'src/system-icons';
+
+export const PATH_ICONS_V3 = 'src/brand2025';

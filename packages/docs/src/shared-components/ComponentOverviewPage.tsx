e1385a6ebe97177956ec4ef21c9e871a6742e4a0
Christian Schilling <christian.schilling@sick.de>
2025-06-03 13:12:56 +0200

    Use classes instead of styles for overview

--- packages/docs/src/shared-components/ComponentOverviewPage.tsx
+++ packages/docs/src/shared-components/ComponentOverviewPage.tsx
@@ -4,15 +4,11 @@ import componentsManifest from 'virtual:vite-plugin-cem/custom-elements-manifest
 // @ts-expect-error virtual is valid
 import stylesManifest from 'virtual:vite-plugin-synergy-styles/custom-elements-manifest';
 import type { Module } from 'custom-elements-manifest/schema.d.ts';
-import React, {
-  useState,
-} from 'react';
+import React from 'react';
 
 import { kebabCase, pascalCase } from 'change-case';
 
 const images = import.meta.glob('../../../assets/src/component-thumbnails/*', { eager: true });
-
-export default { title: 'Component Overview' };
 
 // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
 const componentNames = (componentsManifest.modules as Module[]).map((module) => module.declarations![0].name);
@@ -42,38 +38,41 @@ const imageMap = Object.entries(images).reduce((acc, [filePath, module]) => {
 }, {} as Record<string, string>);
 
 export const ComponentOverviewPage = () => (
-  <div
-    style={{
-      display: 'grid',
-      gap: 'var(--syn-spacing-medium)',
-      gridTemplateColumns: 'repeat(auto-fill, minmax(200px, 1fr))',
-    }}
-  >
-    {Object.entries(imageMap).map(([name, url]) => {
-      const [isHovered, setIsHovered] = useState(false);
+  <div className="syn-doc-component-overview">
+    <style>{`
+      .syn-doc-component-overview {
+        display: grid;
+        gap: var(--syn-spacing-medium);
+        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+      }
+      
+      .syn-doc-component-overview a {
+        border: var(--syn-border-width-medium) solid var(--syn-color-neutral-100);
+        border-radius: var(--syn-border-radius-medium);
+        text-decoration: none;
+        text-align: center;
+      }
+      .syn-doc-component-overview a:hover {
+        border-color: var(--syn-link-color-hover);
+      }
 
-      return (
-        <a
-          key={name}
-          style={{
-            border: `var(--syn-border-width-medium) solid ${isHovered ? 'var(--syn-link-color-hover)' : 'var(--syn-color-neutral-100)'}`,
-            borderRadius: 'var(--syn-border-radius-medium)',
-            cursor: 'pointer',
-          }}
-          tabIndex={0}
-          onMouseEnter={() => setIsHovered(true)}
-          onMouseLeave={() => setIsHovered(false)}
-          href={getUrl(name)}
-        >
-          <figure style={{
-            margin: 0,
-            textAlign: 'center',
-          }}>
-            <figcaption style={{ padding: 'var(--syn-spacing-small) 0' }}>{name}</figcaption>
-            <img src={url} alt={name} className="component"></img>
-          </figure>
-        </a>
-      );
-    })}
+      .syn-doc-component-overview figure {
+        margin: 0;
+        padding: var(--syn-spacing-medium);
+      }
+      
+      .syn-doc-component-overview figcaption {
+        padding: var(--syn-spacing-small) 0;
+      }
+    `}</style>
+
+    {Object.entries(imageMap).map(([name, url]) => (
+      <a href={getUrl(name)} key={name}>
+        <figure>
+          <figcaption>{name}</figcaption>
+          <img src={url} alt={name} className="component"></img>
+        </figure>
+      </a>
+    ))}
   </div>
 );

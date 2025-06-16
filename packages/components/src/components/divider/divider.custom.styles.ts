aa23bdaec7f8acd3e58c825766d534e91ef5fe94
Susanne Kirchner <susanne.kirchner@sick.de>
2025-06-16 11:47:21 +0200

    Try changing color to get a diff

--- packages/components/src/components/divider/divider.custom.styles.ts
+++ packages/components/src/components/divider/divider.custom.styles.ts
@@ -3,7 +3,7 @@ import { css } from 'lit';
 export default css`
   /* Write custom CSS here */
   :host {
-    --color: var(--syn-color-neutral-400);
+    --color: var(--syn-color-neutral-600);
     --width: 2px;
   }
 `;

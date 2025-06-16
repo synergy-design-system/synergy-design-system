bb38193745436ea9d331e0cdcec69775ebf23f9b
Susanne Kirchner <susanne.kirchner@sick.de>
2025-06-16 11:51:01 +0200

    Use an even darker color

--- packages/components/src/components/divider/divider.custom.styles.ts
+++ packages/components/src/components/divider/divider.custom.styles.ts
@@ -3,7 +3,7 @@ import { css } from 'lit';
 export default css`
   /* Write custom CSS here */
   :host {
-    --color: var(--syn-color-neutral-600);
+    --color: var(--syn-color-neutral-900);
     --width: 2px;
   }
 `;

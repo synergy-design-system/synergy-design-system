cb2460def93115c5158cec56c49d02b44d8f4047
Christian Schilling <christian.schilling@sick.de>
2025-06-26 14:47:05 +0200

    syn-select

--- packages/components/code-connect/README.md
+++ packages/components/code-connect/README.md
@@ -17,6 +17,7 @@ This folder holds all figma code connect files used to connect Synergy to Figma.
 - [ ] `figma.enum` will always remove the default property. This makes it hard to use with `figma.className` as for example sizes have to be explicitly added for `syn-link`.
 - [ ] Its not possible to map multiple values to a single enum type. In case of `syn-tooltip` with 4 boolean position sliders there is no way to map this to the property `position="top|bottom|left|right".
 - [ ] We are not able to distinguish between multiple children with the same name, e.g. `figma.children(['child-name'])` and `figma.children('child-name')` are doing the same thing. If you have two layers with the same name (e.g. in `syn-side-nav`), you are not able to fetch the header items for the `defaultSlot` and the second list for the `footer` slot.
+- [ ] Nesting is not possible like in html. For instance, syn-select has a flat list of children. This means instead of `<syn-optgroup><syn-option></syn-optgroup>` you will receive `<syn-optgroup></syn-optgroup><syn-option></syn-option>`.
 
 Skipped:
 
@@ -59,7 +60,7 @@ Skipped:
 - [x] syn-radio-group
 - [ ] syn-range
 - [ ] syn-range-tick
-- [ ] syn-select
+- [x] syn-select
 - [x] syn-side-nav
 - [x] syn-spinner
 - [x] syn-switch
---
+++ packages/components/code-connect/syn-select.figma.ts
@@ -0,0 +1,88 @@
+import figma, { html } from '@figma/code-connect/html';
+import type SynSelect from '../src/components/select/select';
+import type { ValidProperties } from './core/types';
+
+// Dropdown helper
+figma.connect('https://www.figma.com/design/bZFqk9urD3NlghGUKrkKCR/Synergy-Digital-Design-System?node-id=4910-22093&m=dev', {
+  example: ({
+    defaultSlot,
+  }) => html`${defaultSlot}`,
+  props: {
+    defaultSlot: figma.children('*'),
+  },
+});
+
+// Synergy Web Component Connection
+figma.connect('https://www.figma.com/design/bZFqk9urD3NlghGUKrkKCR/Synergy-Digital-Design-System?node-id=4825-30354', {
+  example: ({
+    clearable,
+    defaultSlot,
+    disabled,
+    helpText,
+    label,
+    multiple,
+    open,
+    prefixSlot,
+    placeholder,
+    size,
+    suffixSlot,
+    value,
+  }) => html`
+    <syn-select
+      clearable=${clearable}
+      disabled=${disabled}
+      help-text=${helpText}
+      label=${label}
+      multiple=${multiple}
+      open=${open}
+      placeholder=${placeholder}
+      size=${size}
+      value=${value}
+    >
+      <span slot="prefix">
+        ${prefixSlot}
+      </span>
+      ${defaultSlot}
+      <span slot="suffix">
+        ${suffixSlot}
+      </span>
+    </syn-select>
+  `,
+  imports: [
+    'import { SynSelect } from "@synergy-design-system/components";',
+  ],
+  links: [
+    {
+      name: 'Documentation',
+      url: 'https://synergy-design-system.github.io/?path=/docs/components-syn-select--docs',
+    },
+  ],
+  props: {
+    clearable: figma.boolean('clearable'),
+    defaultSlot: figma.children('_helper/dropdown/syn-select'),
+    disabled: figma.boolean('disabled'),
+    helpText: figma.boolean('help-text', {
+      true: figma.string('└─ content/help-text'),
+    }),
+    label: figma.boolean('label', {
+      true: figma.string('└─ content/label'),
+    }),
+    multiple: figma.boolean('multiple'),
+    open: figma.boolean('open'),
+    placeholder: figma.boolean('placeholder', {
+      true: figma.string('*value'),
+    }),
+    prefixSlot: figma.boolean('prefix', {
+      true: figma.instance('└─ <slot prefix>'),
+    }),
+    suffixSlot: figma.boolean('suffix', {
+      true: figma.instance('└─ <slot suffix>'),
+    }),
+    size: figma.enum('size', {
+      large: 'large',
+      medium: 'medium',
+      small: 'small',
+    }),
+    value: figma.string('*value'),
+  } satisfies ValidProperties<SynSelect, ['default', 'prefix', 'suffix', 'suffix']>,
+});

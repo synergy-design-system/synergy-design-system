dc4db7c174fe06c6ad139ec879141076b70982c8
Christian Schilling <christian.schilling@sick.de>
2025-07-17 11:01:09 +0200

    Removed packages metadata from git

--- packages/_private/mcp/metadata/packages/components/syn-validate/component.styles.ts
+++
@@ -1,9 +0,0 @@
-import { css } from 'lit';
-
-export default css`
-  .validate {
-    display: flex;
-    flex-direction: column;
-    gap: var(--syn-spacing-small);
-  }
-`;
--- packages/_private/mcp/metadata/packages/styles/heading.md
+++
@@ -1,9 +0,0 @@
-
-## Default
-
-
-
-```html
-<span class="syn-heading--3x-large"> This is a default body text </span>
-
-```

94e4a79b1c6b91686a60977f314512b2026de279
Christian Schilling <christian.schilling@sick.de>
2025-08-05 15:25:03 +0200

    ..lint...

--- packages/_private/e2e-demo-test/src/PageObjects/index.ts
+++ packages/_private/e2e-demo-test/src/PageObjects/index.ts
@@ -1,5 +1,5 @@
 export * from './AllComponents.js';
-export * from './AppShell.js'
+export * from './AppShell.js';
 export * from './DemoForm.js';
 export * from './DemoFormValidate.js';
 export * from './PageObject.js';

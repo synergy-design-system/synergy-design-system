2e91ae522a88c7718e7482275d72f8af4a2c2c05
Susanne Kirchner <susanne.kirchner@sick.de>
2025-08-13 15:15:27 +0200

    Add migration documentation

--- packages/_private/react-demo/src/ThemeSwitch.tsx
+++ packages/_private/react-demo/src/ThemeSwitch.tsx
@@ -8,32 +8,33 @@ import { useState } from 'react';
 export const ThemeSwitch = () => {
   const [currentMode, setCurrentMode] = useState('light_mode');
 
-return (
-  <syn-select
-    onsyn-change={e => {
-      const value = (e.target as SynSelect).value as string;
-      setCurrentMode(value.includes('light') ? 'light_mode' : 'dark_mode');
-      setThemeFromOptionString(value);
-    }}
-    placeholder="Select theme to use"
-    size="small"
-    value="2018-light"
-  >
-    <syn-icon name={currentMode} slot="prefix" style={{color: 'var(--syn-color-warning-500)'}}></syn-icon>
+  return (
+    <syn-select
+      onsyn-change={e => {
+        const value = (e.target as SynSelect).value as string;
+        setCurrentMode(value.includes('light') ? 'light_mode' : 'dark_mode');
+        setThemeFromOptionString(value);
+      }}
+      placeholder="Select theme to use"
+      size="small"
+      value="2018-light"
+    >
+      <syn-icon name={currentMode} slot="prefix" style={{ color: 'var(--syn-color-warning-500)' }} />
 
-    {Object
-      .entries(getAvailableThemes())
-      .map(([key, theme]) => (
-        <syn-optgroup label={`${theme.title}`} key={key}>
-          {theme.modes.map(mode => (
-            <syn-option value={`${theme.name}-${mode}`} key={mode}>
-              <syn-icon name={mode.includes('light') ? 'light_mode' : 'dark_mode'} slot="prefix" style={{color: 'var(--syn-color-warning-500)'}}></syn-icon>
-              {theme.title}
-              -
-              {mode}
-            </syn-option>
-          ))}
-        </syn-optgroup>
-      ))}
-  </syn-select>
-)};
+      {Object
+        .entries(getAvailableThemes())
+        .map(([key, theme]) => (
+          <syn-optgroup label={`${theme.title}`} key={key}>
+            {theme.modes.map(mode => (
+              <syn-option value={`${theme.name}-${mode}`} key={mode}>
+                <syn-icon name={mode.includes('light') ? 'light_mode' : 'dark_mode'} slot="prefix" style={{ color: 'var(--syn-color-warning-500)' }} />
+                {theme.title}
+                -
+                {mode}
+              </syn-option>
+            ))}
+          </syn-optgroup>
+        ))}
+    </syn-select>
+  );
+};

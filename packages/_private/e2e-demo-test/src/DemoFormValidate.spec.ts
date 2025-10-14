f8fc7bead53fe3371c1c4391ad0743444ad1afb7
Christian Schilling <christian.schilling@sick.de>
2025-10-14 15:29:38 +0200

    This should finally fix it

--- packages/_private/e2e-demo-test/src/DemoFormValidate.spec.ts
+++ packages/_private/e2e-demo-test/src/DemoFormValidate.spec.ts
@@ -34,19 +34,7 @@ createTestCases(({ name, port }) => {
       expect(states.every((state) => !state)).toBe(true);
     });
 
-    test('Form submit', async ({ page, browserName }) => {
-      // I am not sure why, but only in Chromium the react demo project crashes if we do not
-      // listen to console messages. It works in firefox and webkit without this.
-      // PLEASE LEAVE IT HERE, OBSCURE AS IT MAY BE!
-      // See test outputs for https://github.com/synergy-design-system/synergy-design-system/pull/1035
-      if (browserName === 'chromium' && name === 'react') {
-        page.on('console', msg => {
-          if (msg.type() === 'debug') {
-            console.log(`Browser console [${msg.type()}]:`, msg.text());
-          }
-        });
-      }
-
+    test('Form submit', async ({ page }) => {
       const form = new DemoFormValidate(page, port);
       await form.loadInitialPage();
 
@@ -59,9 +47,9 @@ createTestCases(({ name, port }) => {
       // submit valid form
       await form.submit.click();
 
-      // This has to come before checking for the message,
-      // as react 19.2 will re-render the form and reset its state before the message is shown
-      expect(await form.form.evaluate((f) => (f as HTMLFormElement).checkValidity())).toBe(true);
+      await expect(await form.form.evaluate(
+        (f) => (f as HTMLFormElement).checkValidity(),
+      )).toBe(true);
 
       // Check that the success message is shown
       await expect(form.formStatus).toHaveAttribute('open');

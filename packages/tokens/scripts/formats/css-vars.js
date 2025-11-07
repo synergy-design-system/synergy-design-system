77a96ebb21e2ed5371c4bfb7d0ccb0492b7d94fc
Susanne Kirchner <susanne.kirchner@sick.de>
2025-11-07 11:03:10 +0100

    Update tokens

--- packages/tokens/scripts/formats/css-vars.js
+++ packages/tokens/scripts/formats/css-vars.js
@@ -70,7 +70,6 @@ export const cssVariableFormatter = {
       'typography-color-text-quiet',
       'typography-color-text-quiet-inverted',
       'color-primary-1000',
-      'interactive-background-color-action-hover',
 
       'input-border-color-active',
       'input-icon-icon-clearable-color-active',

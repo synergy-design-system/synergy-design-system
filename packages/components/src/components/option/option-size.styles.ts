53bd655f465b76c2aa7d57449750b99e8fcfb500
Christian Schilling <christian.schilling@sick.de>
2025-12-16 14:58:36 +0100

    fix: 🐛 syn-option current selection does not match Figma (#1134)

--- packages/components/src/components/option/option-size.styles.ts
+++ packages/components/src/components/option/option-size.styles.ts
@@ -5,24 +5,32 @@ export default css`
    * Size adjustments for syn-select and other widgets that use syn-option
    * This allows to share the common settings between all components that
    * should be able to set the size of a syn-option tag.
+   *
+   * Also note that we have to adjust the padding to take the border size into account.
    */
   :host([size="small"]) {
+    --option-inset-border-horizontal: var(--syn-spacing-2x-small);
+    --option-inset-border-vertical: calc(var(--syn-spacing-x-small) - 1px);
     --option-min-height: var(--syn-input-height-small);
-    --option-padding: 0 var(--syn-input-spacing-small);
+    --option-padding: var(--syn-input-spacing-small);
     --option-font-size: var(--syn-input-font-size-small);
     --option-icon-size: var(--syn-spacing-medium);
   }
 
   :host([size="medium"]) {
+    --option-inset-border-horizontal: var(--syn-spacing-2x-small);
+    --option-inset-border-vertical: calc(var(--syn-spacing-x-small) - 1px);
     --option-min-height: var(--syn-input-height-medium);
-    --option-padding: 0 var(--syn-input-spacing-medium);
+    --option-padding: var(--syn-input-spacing-medium);
     --option-font-size: var(--syn-input-font-size-medium);
     --option-icon-size: var(--syn-spacing-large);
   }
 
   :host([size="large"]) {
+    --option-inset-border-horizontal: var(--syn-spacing-2x-small);
+    --option-inset-border-vertical: calc(var(--syn-spacing-x-small) - 1px);
     --option-min-height: var(--syn-input-height-large);
-    --option-padding: 0 var(--syn-input-spacing-large);
+    --option-padding: var(--syn-input-spacing-large);
     --option-font-size: var(--syn-input-font-size-large);
     --option-icon-size: var(--syn-spacing-x-large);
   }

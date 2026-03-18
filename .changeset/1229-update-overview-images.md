db64d0a4513bcbe12ee9dbced909d9f96b23d70a
Christian Schilling <christian.schilling@sick.de>
2026-03-18 10:22:24 +0100

    docs: 📚 Update component overview images (#1236)

---
+++ .changeset/1229-update-overview-images.md
@@ -0,0 +1,6 @@
+---
+"@synergy-design-system/assets": none
+"@synergy-design-system/mcp": none
+---
+
+docs: 📚 update overview images (#1229)
--- packages/docs/src/shared-components/ComponentOverviewPage.tsx
+++ packages/docs/src/shared-components/ComponentOverviewPage.tsx
@@ -6,6 +6,22 @@ import type { Module } from 'custom-elements-manifest/schema.d.ts';
 import React from 'react';
 
 import { kebabCase, pascalCase } from 'change-case';
+import { registerIconLibrary } from '../../../components/src/utilities/icon-library.js';
+import '../../../components/src/components/button/button.js';
+import '../../../components/src/components/card/card.js';
+import '../../../components/src/components/icon/icon.js';
+import { sick2025OutlineIcons } from '../../../assets/dist/index.js';
+
+registerIconLibrary('default', {
+  mutator: svg => svg.setAttribute('fill', 'currentColor'),
+  resolver: (name) => {
+    if (name in sick2025OutlineIcons) {
+      const defaultName = name as keyof typeof sick2025OutlineIcons;
+      return `data:image/svg+xml,${encodeURIComponent(sick2025OutlineIcons[defaultName])}`;
+    }
+    return '';
+  },
+});
 
 const images = import.meta.glob('../../../assets/src/component-thumbnails/*.png', { eager: true });
 
@@ -30,48 +46,66 @@ function getUrl(name: string): string {
   return '#';
 }
 
-const imageMap = Object.entries(images).reduce((acc, [filePath, module]) => {
-  const fileName = filePath.substring(filePath.lastIndexOf('/') + 1, filePath.lastIndexOf('.png'));
-  acc[fileName] = ((module as { default?: string }).default || module) as string;
-  return acc;
-}, {} as Record<string, string>);
+const imageMap = Object
+  .entries(images)
+  .reduce((acc, [filePath, module]) => {
+    const fileName = filePath.substring(filePath.lastIndexOf('/') + 1, filePath.lastIndexOf('.png'));
+    acc[fileName] = ((module as { default?: string }).default || module) as string;
+    return acc;
+  }, {} as Record<string, string>);
 
 export const ComponentOverviewPage = () => (
   <div className="syn-doc-component-overview">
     <style>{`
       .syn-doc-component-overview {
         display: grid;
-        gap: var(--syn-spacing-medium);
-        grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
-      }
-      
-      .syn-doc-component-overview a {
-        border: var(--syn-border-width-medium) solid var(--syn-color-neutral-100);
-        border-radius: var(--syn-border-radius-medium);
-        text-decoration: none;
-        text-align: center;
-      }
-      .syn-doc-component-overview a:hover {
-        border-color: var(--syn-link-color-hover);
+        gap: var(--syn-spacing-2x-large);
+        grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
       }
 
-      .syn-doc-component-overview figure {
-        margin: 0;
-        padding: var(--syn-spacing-medium);
-      }
-      
-      .syn-doc-component-overview figcaption {
-        padding: var(--syn-spacing-small) 0;
+      .syn-doc-component-overview syn-card {
+        &::part(body) {
+          padding: 0;
+        }
+
+        &::part(footer) {
+          padding: 0;
+        }
+        
+        syn-button {
+          --syn-button-border-radius-medium: 0 0 var(--syn-border-radius-medium) var(--syn-border-radius-medium);
+
+          width: 100%;
+          &::part(label) {
+            width: 100%;
+          }
+        }
       }
     `}</style>
 
     {Object.entries(imageMap).map(([name, url]) => (
-      <a href={getUrl(name)} key={name}>
-        <figure>
-          <figcaption>{name}</figcaption>
-          <img src={url} alt={name} className="component"></img>
-        </figure>
-      </a>
+      <syn-card shadow key={name}>
+        <a
+          href={getUrl(name)}
+          slot="image"
+        >
+          <img
+            alt={name}
+            src={url}
+          />
+        </a>
+        <syn-button
+          href={getUrl(name)}
+          slot="footer"
+          variant="filled"
+          size="medium"
+        >
+          <span style={{ textTransform: 'capitalize' }}>
+            {name}
+          </span>
+          <syn-icon name="arrow_forward" slot="suffix" />
+        </syn-button>
+      </syn-card>
     ))}
   </div>
 );

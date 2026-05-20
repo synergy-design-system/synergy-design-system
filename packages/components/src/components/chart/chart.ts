19b42894da9c9d31581d6bd7a5ba694d5ea585b7
Susanne Kirchner <susanne.kirchner@sick.de>
2026-05-20 10:50:34 +0200

    Fix typescript workspace only error

--- packages/components/src/components/chart/chart.ts
+++ packages/components/src/components/chart/chart.ts
@@ -7,6 +7,8 @@ SynChart.define('syn-chart');
 
 declare global {
   interface HTMLElementTagNameMap {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore – false positive: conflict between source and dist declaration
     'syn-chart': SynChart;
   }
 }
